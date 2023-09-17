cbf970bcda5dcc67baa0b4091e8a722b3e934252
So Youn Choi <manghatuhyo@gmail.com>
2023-09-17 20:20:43 +0100

    showed header and album

---
+++ src/AlbumName.jsx
@@ -0,0 +1,23 @@
+import "./AlbumName.css";
+
+export const AlbumName = ({ singleAlbumItem }) => {
+  let singleItemObject = {
+    headingTwo: singleAlbumItem.name,
+    aHref: singleAlbumItem.external_urls.spotify,
+    target: "_blank",
+    rel: "noreferrer noopener",
+    class: "albumName",
+  };
+
+  return (
+    <div className={singleItemObject.class}>
+      <a
+        href={singleItemObject.aHref}
+        target={singleItemObject.target}
+        rel={singleItemObject.rel}
+      >
+        <h2>{singleItemObject.headingTwo}</h2>
+      </a>
+    </div>
+  );
+};
