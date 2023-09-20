9ada3dcffff525f914eac2122095e96b73accf99
Isa Robertini <robertiniisa@gmail.com>
2023-09-20 19:06:52 +0200

    changed play btn

--- src/CoverImage.jsx
+++ src/CoverImage.jsx
@@ -6,7 +6,7 @@ const CoverImage = ({ imageUrl, altText }) => {
             <img className="AlbumImage" src={imageUrl} alt={altText} />
             <div className="middle">
                 <img src="https://img.icons8.com/ios/50/000000/like--v1.png" alt="like--v1" />
-                <img src="https://img.icons8.com/ios/50/000000/play--v1.png" alt="play--v1" />
+                <img src="https://img.icons8.com/ios/50/circled-play.png" alt="circled-play" />
                 <img src="https://img.icons8.com/ios-glyphs/30/ellipsis.png" alt="ellipsis" />
             </div>
         </div>
