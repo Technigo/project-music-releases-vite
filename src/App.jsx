8beba4a1f9a5abdd84b035560f4d1cc1175fe3a4
veronica astrom <v.aastrom@gmail.com>
2023-09-20 16:38:02 +0200

    Initial skeleton of the app created. Need to move everything to their own component file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,70 @@
+import { Fragment } from "react";
 import data from "./data.json";
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <Fragment>
+      <Header />
+      {albumList()}
+    </Fragment>);
+};
+
+
+const albumList = () => {
+  const albumData = data.albums.items;
+
+  return albumData.map(
+    ({ id, name, artists, images }) => {
+  <MusicCard 
+  key = {id}
+  albumCover = {images}
+  albumName = {name}
+  artist = {artists}
+  />
+}
+);
+};
+
+
+
+
+// Header
+const Header = () => {
+  return (
+    <div className="headerWrapper">
+      <h1 className="headerText">New Albums & Singles</h1>
+    </div>
+  );
+}
+
+
+// Parent component
+const MusicCard = () => {
+  return (
+    <div className="musicCard">
+      < AlbumCover />
+      <AlbumInfo />
+    </div >
+  );
+};
+
+// Child component
+const AlbumInfo = () => {
+  return (
+    <div>
+      <p className="albumName">Album name</p>
+      <p className="artistName">artist name</p>
+    </div>
+  )
+};
+
+// Child component
+const AlbumCover = () => {
+  return (
+    <div>
+      Album cover image and alt text
+    </div>
+  );
 };
