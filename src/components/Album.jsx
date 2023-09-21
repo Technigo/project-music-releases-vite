3ad6a77a8311af14dd5c6446ed76c9afb3350533
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-09-22 04:55:10 +0700

    Update Album.jsx
    Update Album.jsx

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -4,11 +4,64 @@
 // import CoverImage from './CoverImage';
 // import Header from './Header';
 
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types'; // Import PropTypes
+// import AlbumName from './AlbumName';
+// import ArtistName from './ArtistName';
+// import CoverImage from './CoverImage';
+// import Header from './Header';
+
+// function Album({ album }) {
+//   const [isHovered, setIsHovered] = useState(false);
+
+//   const handleMouseEnter = () => {
+//     setIsHovered(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setIsHovered(false);
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <div
+//         className={`album ${isHovered ? 'hovered' : ''}`}
+//         onMouseEnter={handleMouseEnter}
+//         onMouseLeave={handleMouseLeave}
+//       >
+//         <CoverImage imageUrl={album.coverImageUrl} />
+//         <Header />
+//         <AlbumName name={album.name} />
+//         <ArtistName artists={album.artists} />
+//         {/* Add Play Button, Favorite Button, and Ellipsis */}
+//         {/* Add external routing links */}
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// // Define PropTypes for the 'album' prop
+// Album.propTypes = {
+//   album: PropTypes.shape({
+//     coverImageUrl: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     artists: PropTypes.arrayOf(
+//       PropTypes.shape({
+//         name: PropTypes.string.isRequired,
+//         externalUrl: PropTypes.string.isRequired,
+//       })
+//     ).isRequired,
+//     // Add more PropTypes for other album properties if necessary
+//   }).isRequired,
+// };
+
+// export default Album;
+
+
 import React, { useState } from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
 import AlbumName from './AlbumName';
 import ArtistName from './ArtistName';
-import CoverImage from './CoverImage';
 import Header from './Header';
 
 function Album({ album }) {
@@ -24,31 +77,45 @@ function Album({ album }) {
 
   return (
     <React.Fragment>
-      <div
-        className={`album ${isHovered ? 'hovered' : ''}`}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <CoverImage imageUrl={album.coverImageUrl} />
-        <Header />
-        <AlbumName name={album.name} />
-        <ArtistName artists={album.artists} />
-        {/* Add Play Button, Favorite Button, and Ellipsis */}
-        {/* Add external routing links */}
-      </div>
+    <div
+      className={`album ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {album.images.map((image, index) => (
+        <img
+          key={index}
+          className="cover-image"
+          src={image.url}
+          alt={`Album Cover ${index + 1}`}
+        />
+      ))}
+      <Header />
+      <AlbumName name={album.name} />
+      <ArtistName artists={album.artists} />
+      {/* Add Play Button, Favorite Button, and Ellipsis */}
+      {/* Add external routing links */}
+    </div>
     </React.Fragment>
   );
 }
 
-// Define PropTypes for the 'album' prop
 Album.propTypes = {
   album: PropTypes.shape({
-    coverImageUrl: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        url: PropTypes.string.isRequired,
+        height: PropTypes.number.isRequired,
+        width: PropTypes.number.isRequired,
+      })
+    ).isRequired,
     name: PropTypes.string.isRequired,
     artists: PropTypes.arrayOf(
       PropTypes.shape({
         name: PropTypes.string.isRequired,
-        externalUrl: PropTypes.string.isRequired,
+        external_urls: PropTypes.shape({
+          spotify: PropTypes.string.isRequired,
+        }).isRequired,
       })
     ).isRequired,
     // Add more PropTypes for other album properties if necessary
@@ -56,3 +123,4 @@ Album.propTypes = {
 };
 
 export default Album;
+
