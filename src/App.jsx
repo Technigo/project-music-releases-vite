b600832e9729f5e9b569a7d291a18b26ce1f632d
Elin Olausson <140396742+Soygirt@users.noreply.github.com>
2023-09-21 22:38:24 +0200

    first commit, extracted albums.. maybe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import data from "./data.json";
+//import { Header } from ./Album ?????
+//import { Album } from "./Components/Album";
 
-console.log(data);
+//console.log(data);
 
+const albums = data.albums.items
+//console.log(albums);
+
+//const artists = data.albums.artists
+//console.log(artists);
+
+const extractedAlbums = albums.map((album) => {
+
+  const artistName = album.artists[0].name;
+  const albumName = album.name;
+  const coverImage = album.images[0].url;
+
+  return {
+    artistName,
+    albumName,
+    coverImage,
+  };
+
+});
+
+console.log(extractedAlbums);
+
+
+//header to export? Make one?
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return <div>
+    Hallo!
+  </div>;
 };
---
+++ src/Components/Album.jsx
@@ -0,0 +1,11 @@
+//import React from 'react';
+//import { AlbumName } from "./AlbumName";
+//import { ArtistName } from "./ArtistName";
+//import { CoverImage } from "./CoverImage";
+
+
+export default function Album() {
+    return (
+        <div>Album</div>
+    )
+}
