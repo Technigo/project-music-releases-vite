940c70f6fcdb9ed9c6ccb3f36c4afeae77ca0ae3
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-09-21 20:51:37 +0200

    -m small cganges

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import AlbumName from './AlbumName';
 import ArtistName from './ArtistName';
 import CoverImage from './CoverImage';
-import Header from './Header';
 import '../index.css';
-
 
 const Album = ({ albumName, artistNames, coverImages, externalUrls }) => {
   return (
     <div className="album">
-      <Header /> {/* Include the Header component here */}
       <CoverImage images={coverImages} />
       <AlbumName albumName={albumName} externalUrl={externalUrls.spotify} />
       <div className="artists">
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import jsonData from '../data.json'; // Import your JSON data
+import jsonData from '../data.json';
 import Album from './Album';
+import Header from './Header'; // if this is a child to the album shouldn't that be in there, if i put it there it's visible over every album
 import '../index.css';
 
 const App = () => {
   return (
     <div className="app">
-      <div className="album-container"> {/* Add a container div */}
+      <Header /> {/* Include the Header component at the top of the page */}
+      <div className="album-container">
         {jsonData.albums.items.map((album) => (
           <Album
-            key={album.id} // Use a unique identifier as the "key" prop
+            key={album.id}
             albumName={album.name}
             artistNames={album.artists.map((artist) => artist.name).join(', ')}
             coverImages={album.images}
