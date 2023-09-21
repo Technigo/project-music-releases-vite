2e42e813638b55627cb04db80a8f9bce06d02f4e
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-09-21 08:57:03 +0200

    -m added media response

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,17 @@ import '../index.css';
 const App = () => {
   return (
     <div className="app">
-      {jsonData.albums.items.map((album) => (
-        <Album
-          key={album.id} // Use a unique identifier as the "key" prop
-          albumName={album.name}
-          artistNames={album.artists.map((artist) => artist.name).join(', ')}
-          coverImages={album.images}
-          externalUrls={{ spotify: album.external_urls.spotify, artist: album.artists[0].external_urls.spotify }}
-        />
-      ))}
+      <div className="album-container"> {/* Add a container div */}
+        {jsonData.albums.items.map((album) => (
+          <Album
+            key={album.id} // Use a unique identifier as the "key" prop
+            albumName={album.name}
+            artistNames={album.artists.map((artist) => artist.name).join(', ')}
+            coverImages={album.images}
+            externalUrls={{ spotify: album.external_urls.spotify, artist: album.artists[0].external_urls.spotify }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
