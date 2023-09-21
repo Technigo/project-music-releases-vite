a4ee8cb32e409b4237364e87ff01b4c26f3c668f
Beckie Morton <139934447+BeckieMorton@users.noreply.github.com>
2023-09-21 20:22:12 +1000

    Update AlbumImage.jsx

--- src/components/AlbumImage.jsx
+++ src/components/AlbumImage.jsx
@@ -5,9 +5,9 @@ export const AlbumImage = ({ image, name, link }) => {
             <div className="album-cover">
                 <img className="album-image" src={image.url} alt={'Cover art for albumn: ${name}'} />
                 <div className="three-hover-buttons">
-                    <img className="button-heart" src="./src/assets/icons/heart.svg" alt="Heart button" />
-                    <img className="button-play" src="./src/assets/icons/play.svg" alt="Play button" />
-                    <img className="button-dots" src="./src/assets/icons/dots.svg" alt="Dots button" />
+                    <img className="button-heart" src="/src/assets/icons/heart.svg" alt="Heart button" />
+                    <img className="button-play" src="/src/assets/icons/play.svg" alt="Play button" />
+                    <img className="button-dots" src="/src/assets/icons/dots.svg" alt="Dots button" />
                 </div>
             </div >
         </div >
