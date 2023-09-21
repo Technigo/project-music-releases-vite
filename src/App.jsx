8b737595d0467976f5923a4660383cbcdb2a0b7b
Breanna Sand <breanna.sand8@gmail.com>
2023-09-21 14:53:47 +0200

    added component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Album } from "./components/Album";
 import data from "./data.json";
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    //<Album/>
+    <div>Find me in src/app.jsx!</div>
+  );
 };
