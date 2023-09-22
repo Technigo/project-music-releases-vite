8a334431adbe3b2f9b341fa6f8f4652dcc905ff2
elbines <141184135+elbines@users.noreply.github.com>
2023-09-22 22:19:08 +0200

    grids and started header, header css

--- src/Album.jsx
+++ src/Album.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import ArtistName from "./Components/ArtistName/ArtistName";
 import AlbumName from "./Components/AlbumName/AlbumName";
 import CoverImage from "./Components/CoverImage/CoverImage";
-//import { Header } from "./Components/Header/Header";
+import Header from "./Components/Header/Header";
 import data from "./data.json"; //im really not sure where i need to add data.json
 
 //{album.images[0].url} change [0] if i want another image that is different size
+//explanation (use of map) privateNote-1:
 const Album = ({ data }) => {
     return (
-        <section>
+        <section className="album-wrapper">
+            <Header />
             {data.map((album, index) => (
                 <div key={index}>
                     <div>
-                        <CoverImage url={album.images[0].url} alt={album.name} />
+                        <CoverImage url={album.images[1].url} alt={album.name} />
                     </div>
                     <br />
                     <div>
@@ -32,5 +34,6 @@ export default Album;
 
 
 
+//why album.jsx   - data.json - JSON file containing 1 key called `"albums"` containing keys mimiquing a Spotify API object endpoint.
 
 //PREVIOUS COMMENT First I passed everything from the parent. I think I managed to change it, and its now passed trough the child componants. 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styling from './Header.module.css';
 
-export function Header({ text }) {
-    return <h1>{text}</h1>;
+const Header = () => {
+    return (
+        <header className={styling.header}>
+            <h1>hello</h1>
+        </header>
+    );
 }
+
+export default Header;
