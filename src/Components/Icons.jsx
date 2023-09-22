d35b8d526f7569a3799d527335b5faafcce046f3
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-09-22 14:52:25 +0200

    fixing icon paths

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -3,9 +3,9 @@ import "./Icons.css";
 export const Icons = () => {
     return (
         <div className="icon-container">
-            <img className="icon heart" src="./public/assets/heart.svg" alt="A heart icon" />
-            <img className="icon play" src="./public/assets/play.svg" alt="A play icon" />
-            <img className="icon dots" src="./public/assets/dots.svg" alt="Three dots icon" />
+            <img className="icon heart" src="./assets/heart.svg" alt="A heart icon" />
+            <img className="icon play" src="./assets/play.svg" alt="A play icon" />
+            <img className="icon dots" src="./assets/dots.svg" alt="Three dots icon" />
         </div>
     )
 }
