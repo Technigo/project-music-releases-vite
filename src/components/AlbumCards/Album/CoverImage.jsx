bb6549a8c8f8ded7736c469d47d41ccfda140ff5
LauraLyckholm <laura.lyckholm@gmail.com>
2023-09-22 14:46:23 +0200

    test

--- src/components/AlbumCards/Album/CoverImage.jsx
+++ src/components/AlbumCards/Album/CoverImage.jsx
@@ -1,12 +1,15 @@
+
+
 export const CoverImage = ({ image }) => {
     // Functional component that destructures the prop image, and sets it as the source of the image tag.
     return (
       <div className="cover-container">
         <img className="cover-image" src={image.url} alt="Album Cover"/>
         <div className="icon-container hidden">
-          <img id="favorite-icon" src="public/icons/heart.svg" alt="Favorite Icon" />
-          <img id="play-icon" src="public/icons/play.svg" alt="Play Icon" />
-          <img id="more-info-icon" src="public/icons/dots.svg" alt="More Info Icon" />
+          <img id="favorite-icon" src="./icons/heart.svg" alt="Favorite Icon" />
+          <img src="../../../../public/icons/heart.svg" alt="" />
+          <img id="play-icon" src="./icons/play.svg" alt="Play Icon" />
+          <img id="more-info-icon" src="./icons/dots.svg" alt="More Info Icon" />
         </div>
       </div>
       )
