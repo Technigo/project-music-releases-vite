6deca57c06fa177efc1e1d3173fb96d89d2debfd
Sheryll-Ann <shey.pc@gmail.com>
2023-09-22 22:33:47 +0200

    Moved svg icons to public + modif CoverImage.jsx

--- src/components/albums/album-child-component/CoverImage.jsx
+++ src/components/albums/album-child-component/CoverImage.jsx
@@ -7,21 +7,21 @@ export const CoverImage = ({ url }) => {
                     type="button"
                     className="icon heart">
                     <img
-                        src="src/assets/heart.svg"
+                        src="public/heart.svg"
                         alt="icon dots" />
                 </button>
                 <button
                     type="button"
                     className="icon play">
                     <img
-                        src="src/assets/play.svg"
+                        src="public/play.svg"
                         alt="icon dots" />
                 </button>
                 <button
                     type="button"
                     className="icon ellipsis">
                     <img
-                        src="src/assets/dots.svg"
+                        src="public/dots.svg"
                         alt="icon dots" />
                 </button>
             </span>
