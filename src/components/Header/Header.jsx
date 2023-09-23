ca4ec9065170c089728775b976b510a0f1499793
Julia Holm <juliaholm@julias-mbp.home>
2023-09-23 21:32:40 +0200

    -

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.css";
+import SpotifyLogo from "../../assets/spotify-logo.png";
 
 export const Header = () => {
   return (
     <>
       <header>
-        <img src="./public/spotify-logo.png" alt="Spotify" />
+        <img src={SpotifyLogo} alt="Spotify" />
         <h1>New albums & singles</h1>
       </header>
     </>
