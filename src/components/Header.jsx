047fb10ea04b8aa5ed831c87f28a3c3f41865db8
Sheryll-Ann <shey.pc@gmail.com>
2023-09-23 11:08:08 +0200

    Personalised header msg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className="header">
             <h1><strong>New Albums & Singles</strong></h1>
-            <p>Technigo w5 music project</p>
+            <p>Technigo- Sheryll-Ann music project</p>
         </div>
     )
 }
