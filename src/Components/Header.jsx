39b5e2c6988d96acf1740e578e331cc2e6a26207
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-09-23 19:26:29 +0200

    Added hover effect on images, moved icons to public folder

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div>
         <h1 className="header">New Albums & Singles</h1>  
-        <p className="header-p">Technigo Project week 5 by Susanne Ekenheim</p>
+        <p className="header-p">Technigo React Project by Susanne Ekenheim</p>
     </div>
   );
 };
