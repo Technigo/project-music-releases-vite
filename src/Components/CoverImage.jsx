719de302f37f9c9da338c4ab84c917dfe9707d0f
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-09-23 22:50:06 +0200

    added the 3 icons and changed their color

--- src/Components/CoverImage.jsx
+++ src/Components/CoverImage.jsx
@@ -1,8 +1,18 @@
-
 const CoverImage = ({coverImage}) => {
     return (
-         <div className="coverImage-wrapper">
-            <img src={coverImage} alt="Cover image" />
+         <div className="coverimage-wrapper">
+            <img src={coverImage} alt="Album cover" />
+                <span className="icon-container">
+                    <button type="button" className="heart-icon"/>
+                        <img src="public/Icons/heart.svg" alt="Heart Icon" 
+                        className="white-icon"/>
+                    <button type="button" className="play-icon"/>
+                        <img src="public/Icons/play.svg" alt="Play Icon" 
+                        className="white-icon"/>
+                    <button type="button" className="dots-icon"/>
+                        <img src="public/Icons/dots.svg" alt="Dots Icon" 
+                        className="white-icon"/>
+                </span>
         </div>
         )
 }
