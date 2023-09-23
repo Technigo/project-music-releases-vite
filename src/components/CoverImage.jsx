80813e9c37c3ef285fb818c2ce46d9dc08a83981
Klaudia Wróblewska <kwroblewska0410@gmail.com>
2023-09-23 12:09:46 +0200

    added icons

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable react/prop-types */
-
+import { IconsButtons } from "./IconsButtons";
 export const CoverImage = ({ images }) => {
   let imageUrl = images; 
 
-  return <img src={imageUrl} alt="Album Cover" className="coverImage" />;
+  return (
+    <>
+  <img src={imageUrl} alt="Album Cover" className="coverImage" />
+  <IconsButtons />
+  </>
+  )
 }
 
---
+++ src/components/IconsButtons.jsx
@@ -0,0 +1,11 @@
+
+/* eslint-disable react/prop-types */
+export const IconsButtons = () => {
+  return (
+    <div className="icon-container hidden">
+    <img id="favorite-icon" src="./icons/heart.svg" alt="Favorite Icon" />
+    <img id="play-icon" src="./icons/play.svg" alt="Play Icon" />
+    <img id="more-info-icon" src="./icons/dots.svg" alt="More Info Icon" />
+  </div>
+  )
+}
