ecabd657230a8a2fac52170e43932d7a257ff73d
Anna Robertsson <robertsson_anna@hotmail.com>
2023-09-24 00:56:31 +0200

    buttons almost complete

--- src/components/Album/CoverImage/CoverImage.jsx
+++ src/components/Album/CoverImage/CoverImage.jsx
@@ -3,8 +3,28 @@ import "./CoverImage.css";
 // This component takes a prop named 'coverImage', which is expected to be a URL to the album cover image. It uses destructuring to directly extract the 'coverImage' prop from the props object. It will render an 'img' element displaying the album cover image.
 export const CoverImage = ({ coverImage }) => {
     return (
-      <div className="cover-images">
-        <img src={coverImage} className="album-cover" alt="Cover" />          
+      <div className="cover-image">
+        <img src={coverImage} className="album-cover" alt="Cover" />  
+        <div className="icon-wrapper">
+          <button type="button" className="heart-icon-button">
+            <img src="src/assets/icons/heart.svg"
+            alt="Heart Icon"
+            className="white-icon"
+            />
+            </button>
+          <button type="button" className="play-icon-button custom-play-button">
+            <img src="src/assets/icons/play.svg"
+            alt="Play Icon"
+            className="white-icon"
+            />
+            </button>
+          <button type="button" className="dots-icon-button">
+            <img src="src/assets/icons/dots.svg"
+            alt="Dots Icon"
+            className="white-icon"
+            />
+            </button>    
+        </div>    
       </div>
     );
   };
