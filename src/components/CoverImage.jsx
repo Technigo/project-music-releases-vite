2cc8d0adcfa1fd765fea2ef73dd80c8b9391b672
olgalepisto <olga.lepisto@gmail.com>
2023-09-24 17:50:19 +0300

    moved icons' directory for deployment

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -5,9 +5,9 @@ export const CoverImage = ({ cover }) => {
     <div className="album-cover">
         <img src={cover[1].url} className="cover-image" alt="Album cover image" />
         <div className="icons">
-                <img src="./src/icons/heart.png" className="like" alt="like button" />
-                <img src="./src/icons/play.png" className="play" alt="play button" />
-                <img src="./src/icons/dots.png" className="dots" alt="more info" />
+                <img src="./src/components/heart.png" className="like" alt="like button" />
+                <img src="./src/components/play.png" className="play" alt="play button" />
+                <img src="./src/components/dots.png" className="dots" alt="more info" />
         </div>
     </div>
     )
