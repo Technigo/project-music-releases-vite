264317559d1e360e0a779c317840568272c99fa0
veronica astrom <v.aastrom@gmail.com>
2023-09-24 09:25:04 +0200

    adjusted styling for all components. Worked on the icons that wouldn't show before by adding the svg files to the public folder. The icons now appear

--- src/components/AlbumCover.jsx
+++ src/components/AlbumCover.jsx
@@ -3,25 +3,25 @@ import ".././css_Components/AlbumCover.css";
 
 export const AlbumCover = ({ albumCoverItem }) => {
 
-let albumCoverObject = {
-coverImgUrl: albumCoverItem.images[0].url,
-coverImageAltText: albumCoverItem.name,
-};
+  let albumCoverObject = {
+    coverImgUrl: albumCoverItem.images[0].url,
+    coverImageAltText: albumCoverItem.name,
+  };
 
   return (
     <div className="albumCover">
       <img src={albumCoverObject.coverImgUrl} alt={`This is the cover image for the album ${albumCoverObject.coverImageAltText}`} />
       <span className="icon-wrapper">
-            <button type="button" className="icon heartIcon">
-                <img src="./assets/heart.svg" alt="heart icon" />
-            </button>
-            <button type="button" className="icon playIcon">
-                <img src="./assets/play.svg" alt="play icon" />
-            </button>
-            <button type="button" className="icon dotsIcon">
-                <img src="./assets/dots.svg" alt="three dots icon" />
-            </button>
-        </span>
+        <button type="button" className="icon heartIcon">
+          <img src="./heart.svg" alt="heart icon" />
+        </button>
+        <button type="button" className="icon playIcon">
+          <img src="./play.svg" alt="play icon" />
+        </button>
+        <button type="button" className="icon dotsIcon">
+          <img src="./dots.svg" alt="three dots icon" />
+        </button>
+      </span>
     </div>
   );
 };
