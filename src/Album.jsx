414ea782142a1e093fc7facbc10c00070b5c1d40
elbines <141184135+elbines@users.noreply.github.com>
2023-09-24 20:30:30 +0200

    found icons folder, fixed artists &

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,32 +1,42 @@
+
+import Header from "./Components/Header/Header";
 import React from "react";
 import ArtistName from "./Components/ArtistName/ArtistName";
 import AlbumName from "./Components/AlbumName/AlbumName";
 import CoverImage from "./Components/CoverImage/CoverImage";
-import Header from "./Components/Header/Header";
-import data from "./data.json"; //im really not sure where i need to add data.json
 
-//{album.images[0].url} change [0] if i want another image that is different size
-//album.name: I think it will read out loud the actual name of the album f.eks with screen readers. 
-//explanation (use of map) privateNote-1:
 const Album = ({ data }) => {
     return (
         <div>
             <Header />
             <section className="album-wrapper">
-
                 {data.map((album, index) => (
                     <div key={index}>
                         <div>
-                            <CoverImage url={album.images[1].url} alt={album.name} />
-                        </div>
-                        <br />
-                        <div>
-                            <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer"><AlbumName name={album.name} /></a>
-                        </div>
-                        <br />
-                        <div>
-                            <a href={album.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer" ><ArtistName name={album.artists[0].name} />
-                            </a>
+                            <div>
+                                <CoverImage url={album.images[1].url} alt={album.name} />
+                            </div>
+                            <div>
+                                <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                                    <AlbumName name={album.name} />
+                                </a>
+                            </div>
+                            <h2>{album.album}</h2>
+                            {album.artists.map((artist, artistIndex) => (
+                                <span key={artistIndex}>
+                                    <a
+                                        href={artist.external_urls.spotify}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <ArtistName
+                                            name={artist.name}
+                                            index={artistIndex}
+                                            totalArtists={album.artists.length}
+                                        />
+                                    </a>
+                                </span>
+                            ))}
                         </div>
                     </div>
                 ))}
@@ -35,10 +45,23 @@ const Album = ({ data }) => {
     );
 };
 
+export default Album;
+
+
+//Artist name wrapped in span instead of div. this keeps them on the same line
+
+
+//{album.images[0].url} change [0] if i want another image that is different size
+//album.name: I think it will read out loud the actual name of the album f.eks with screen readers. 
+//explanation (use of map) privateNote-1:
+
+
+
+
+
 //changed: <AlbumName name={album.name} /
 //rel="noopener noreferrer"> safety reasons as i understand it? target blank: new window
 //changed from ul and li to section and div. not sure whats best. Might need to add more than one artist later. 
-export default Album;
 
 
 
--- src/Components/ArtistName/ArtistName.jsx
+++ src/Components/ArtistName/ArtistName.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import styling from './ArtistName.module.css';
 
+const ArtistName = ({ name, index, totalArtists }) => {
+    const generateDelimiter = (index, length) => {
+        if (index < length - 1) {
+            return " & ";
+        }
+        return "";
+    };
+
+    return (
+        <span className={styling.ArtistName}>
+            {name}
+            {generateDelimiter(index, totalArtists)}
+        </span>
+    );
+};
+
+export default ArtistName;
+
+
+
+/*import React from "react";
+import styling from './ArtistName.module.css';
+
 const ArtistName = ({ name }) => {
     return (<span className={styling.ArtistName}>{name}</span>
     );
 };
 
 export default ArtistName;
-
+*/
