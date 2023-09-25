26b95fbc8714b0913a9f0753d84cf2a6865c6072
olgalepisto <olga.lepisto@gmail.com>
2023-09-25 14:29:41 +0300

    updated readme

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -5,9 +5,9 @@ export const CoverImage = ({ cover }) => {
     <div className="album-cover">
         <img src={cover[1].url} className="cover-image" alt="Album cover image" />
         <div className="icons">
-                <img src="./public/heart.png" className="like" alt="like button" />
-                <img src="./public/play.png" className="play" alt="play button" />
-                <img src="./public/dots.png" className="dots" alt="more info" />
+                <img src="/heart.png" className="like" alt="like button" />
+                <img src="/play.png" className="play" alt="play button" />
+                <img src="/dots.png" className="dots" alt="more info" />
         </div>
     </div>
     )
