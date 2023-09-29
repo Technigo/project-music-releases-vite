e61b50f63afb8b73e2fd35dc62ba6ecf560cb636
Sakura Tanaka <sito6496t@google.com>
2023-09-29 11:12:50 +0200

    opacity is changed

--- src/Components/CoverImage/CoverImage.jsx
+++ src/Components/CoverImage/CoverImage.jsx
@@ -1,22 +1,31 @@
-import {useState } from "react"
-import style from "./CoverImage.module.css"
-export const CoverImagage=(url)=>{
+import { useState } from "react";
+import style from "./CoverImage.module.css";
+export const CoverImagage = (url) => {
+  const imageUrl = url.url[0].url;
 
-   const imageUrl=url.url[0].url
+  const [isShown, setIsShown] = useState(false);
 
-const [isShown,setIsShown]=useState(false)
-
-
-
-
-    return (
+  return (
     <div className={style.image_container}>
-    <img className={`${style.coverImage} ${isShown ? style.dark : ""}`} src={imageUrl}  />
-    <div className={style.icon_wrapper}  onMouseOver={() => setIsShown(true)}  onMouseLeave={() => setIsShown(false)}>
-
-    <img className={`${style.icon_like}  ${isShown ? style.visible : ""}`} src="/assets/icons/like.png"/>
-    <img className={`${style.icon_play}  ${isShown ? style.opacity : ""} ${isShown ? style.visible : ""}`} src="/assets/icons/play.png"/>
-    <img className={`${style.icon_more}  ${isShown ? style.visible : ""}`} src="/assets/icons/more.png"/>
+      <img className={`${style.coverImage} ${isShown ? style.dark : ""}`} src={imageUrl} />
+      <div
+        className={style.icon_wrapper}
+        onMouseOver={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        <img
+          className={`${style.icon_like}  ${isShown ? style.visible : ""}`}
+          src="/assets/icons/like.png"
+        />
+        <img
+          className={`${style.icon_play}  ${isShown ? style.visible : ""}`}
+          src="/assets/icons/play.png"
+        />
+        <img
+          className={`${style.icon_more}  ${isShown ? style.visible : ""}`}
+          src="/assets/icons/more.png"
+        />
+      </div>
     </div>
-          
-    </div>)}
+  );
+};
