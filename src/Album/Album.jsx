b326e21ea293ef5fa1d604541dae2024fe71f3cb
Shiqwan <ulrika.galnander@gmail.com>
2023-10-07 20:52:53 +0200

    Electric bogaloo

--- src/Album/Album.jsx
+++ src/Album/Album.jsx
@@ -7,16 +7,16 @@ import { CoverImage } from "../Album/CoverImage/CoverImage";
 export const Album = ({ albumsArray }) => {
   return (
     <section className="album-container">
-      {albumsArray.map((MappedOutItem) => (
-        <div className="album-card" key={MappedOutItem.id}>
-          <CoverImage coverImage={MappedOutItem.images[0].url} />
+      {albumsArray.map((MappedAlbum) => (
+        <div className="album-card" key={MappedAlbum.id}>
+          <CoverImage coverImage={MappedAlbum.images[0].url} />
           <AlbumName
-            albumName={MappedOutItem.name}
-            albumURL={MappedOutItem.external_urls.spotify}
+            albumName={MappedAlbum.name}
+            albumURL={MappedAlbum.external_urls.spotify}
           />
           <ArtistNames
-            artistsArray={MappedOutItem.artists}
-            artistURL={MappedOutItem.external_urls.spotify}
+            artistsArray={MappedAlbum.artists}
+            artistURL={MappedAlbum.external_urls.spotify}
           />
         </div>
       ))}
