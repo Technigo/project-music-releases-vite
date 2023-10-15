5d844eeb0f649873646653f1d053f4cba9283b75
Linnea Johansson <Linneajoh@gmail.com>
2023-10-15 20:26:36 +0200

    styling

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header">
-        <h1 className="header-text">New music releases</h1>
+        <h1 className="header-text">New Music Releases</h1>
     </div>
   )
 }
