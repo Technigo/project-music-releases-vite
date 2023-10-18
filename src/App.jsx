ae16386277a435b810b0c68baa0d2abd3970bf9a
psterner <psterner@hotmail.com>
2023-10-18 14:10:07 +0200

    Import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import "./index.css";
+import { Header } from "./components/Header";
 import data from "./data.json";
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <header>
+      <Header />
+    </header>
+  );
 };
