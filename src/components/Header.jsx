3e3a676c55ba1adf1507878334b1b98af84734d9
psterner <psterner@hotmail.com>
2023-10-18 14:09:23 +0200

    Add hamburger, title and spotify-icon to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <>
+      <img src="./src/assets/icons/menu-icon.svg" alt="Menu-Icon" />
+      <h1>Music Releases from </h1>
+      <img src="./src/assets/icons/spotify-icon.svg" alt="Spotify-Icon" />
+    </>
+  );
+};
