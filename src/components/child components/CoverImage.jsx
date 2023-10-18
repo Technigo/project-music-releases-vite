5b307d45225085f74ed210c0b51d123350d2a3a5
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-18 11:29:10 +0200

    Diana fix conflicts after merge (add album cover + album url) (#2)
    * start on project structure
    
    * complete project folder structure + render album info
    
    * add album url + responsiveness

--- src/components/child components/CoverImage.jsx
+++ src/components/child components/CoverImage.jsx
@@ -1,20 +1,20 @@
 
-export const CoverImage = ({ images }) => {
+export const CoverImage = ({ images, albumUrl }) => {
+    
+    const selectedImage = images.filter(
+        ({height}) => height === 300
+    )
+    const selectedUrl = selectedImage[0].url
+    //console.log(selectedImage);
     return (
         <div className="albumCover">
-            <span>Cover Image:</span>
-            {images.map((img, key) => {
-                <ul key={key}>
-                    {" "}
-                    <li key={img}>{img}</li>
-                </ul>
-            })}
+           <a href={albumUrl}><img src={selectedUrl} alt="Album cover" /></a>
         </div>
     );
 };
 
 CoverImage.defaultProps = {
-    images: "No cover image",
+    //images: ["No cover image"]
 };
 
 //images[1].ulr.map
