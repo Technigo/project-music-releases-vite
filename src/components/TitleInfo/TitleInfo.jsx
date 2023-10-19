7491e0f1331aea143f52b0229872457dd1ef5ab3
SETI108 <seti108@gmail.com>
2023-10-19 15:19:09 +0200

    Add defaultProps for all variables + CSS class

--- src/components/TitleInfo/TitleInfo.jsx
+++ src/components/TitleInfo/TitleInfo.jsx
@@ -1,16 +1,22 @@
+import "./TitleInfo.css";
+
 export const TitleInfo = ({ name, artists, external_urls }) => {
   return (
     <>
       <a href={external_urls}>{name}</a>
-      {artists.map((artist, index) => (
-        <div key={index}>
-          <a href={artist.external_urls.spotify}>{artist.name}</a>
-        </div>
-      ))}
+      <div className="artist_span">
+        {artists.map((artist, index) => (
+          <a key={index} href={artist.external_urls.spotify}>
+            {artist.name}
+          </a>
+        ))}
+      </div>
     </>
   );
 };
 
 TitleInfo.defaultProps = {
   name: "Album title is missing",
+  artist: "Artists are missing",
+  external_urls: "Link is missing",
 };
