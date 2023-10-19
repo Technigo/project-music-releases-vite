b8e4660e0a886a857eade3dc56002fb252afdc39
SETI108 <seti108@gmail.com>
2023-10-19 12:03:17 +0200

    Remake main app page into columns for albums and singles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import data from "./data.json";
 
 export const App = () => {
   const albums = data.albums.items;
+  const onlyAlbums = albums.filter((album) => album.album_type === "album");
+  const onlySingles = albums.filter((album) => album.album_type === "single");
 
-  const renderAlbums = () =>
-    albums.map(({ id, name, artists, external_urls, images }) => {
+  const renderAlbumComponent = (albumOrSingleArray) =>
+    albumOrSingleArray.map(({ id, name, artists, external_urls, images }) => {
       return (
         <Album
           key={id}
@@ -19,14 +21,27 @@ export const App = () => {
       );
     });
 
-  const renderContent = renderAlbums();
+  const renderedAlbums = renderAlbumComponent(onlyAlbums);
+  const renderedSingles = renderAlbumComponent(onlySingles);
 
   return (
     <>
       <header>
         <Header />
       </header>
-      <section className="albumOuter">{renderContent}</section>
+      <section className="main__albums_singles">
+        <section className="main__albums_singles_column">
+          <h2>Albums</h2>
+          <div className="main__albums_singles_container">{renderedAlbums}</div>
+        </section>
+        <section className="main__albums_singles_column">
+          <h2>Singles</h2>
+          <div className="main__albums_singles_container">
+            {renderedSingles}
+          </div>
+        </section>
+      </section>
+      <section className="albumOuter"></section>
     </>
   );
 };
