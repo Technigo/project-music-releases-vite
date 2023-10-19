3a3b5363d7333c3f81926cc306743b538272c4f8
SETI108 <seti108@gmail.com>
2023-10-19 16:56:48 +0200

    Add CSS classes for styling the playcontroller

--- src/components/Controller/PlayControll.jsx
+++ src/components/Controller/PlayControll.jsx
@@ -6,14 +6,14 @@ import "./PlayControll.css";
 export const PlayControll = () => {
   return (
     <div className="playcontroll__flex_container">
-      <div className="playcontroll__image_div">
-        <img src={HeartIcon} alt="heart icon" width="50" />
+      <div className="playcontroll__image_div playcontroll__image_div_heart">
+        <img src={HeartIcon} alt="heart icon" width="100%" />
+      </div> 
+      <div className="playcontroll__image_div playcontroll__image_div_play">
+        <img src={PlayIcon} alt="play icon" width="100%" />
       </div>
-      <div className="playcontroll__image_div">
-        <img src={PlayIcon} alt="play icon" width="50" />
-      </div>
-      <div className="playcontroll__image_div">
-        <img src={DotsIcon} alt="dots icon" width="50" />
+      <div className="playcontroll__image_div playcontroll__image_div_dots">
+        <img src={DotsIcon} alt="dots icon" width="100%" />
       </div>
     </div>
   );
