2afb55be0639bee6b5cd3907ead294c8d1abf6e6
SETI108 <seti108@gmail.com>
2023-10-19 17:45:06 +0200

    Add CSS class to title for styling

--- src/components/TitleInfo/TitleInfo.jsx
+++ src/components/TitleInfo/TitleInfo.jsx
@@ -3,11 +3,13 @@ import "./TitleInfo.css";
 export const TitleInfo = ({ name, artists, external_urls }) => {
   return (
     <>
-      <a href={external_urls}>{name}</a>
+      <a className="title_span" href={external_urls}>
+        {name}
+      </a>
       <div className="artist_span">
         {artists.map((artist, index) => (
           <a key={index} href={artist.external_urls.spotify}>
-            {artist.name}
+            <span>{artist.name}</span>
           </a>
         ))}
       </div>
