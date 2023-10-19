9d52412ed237c1522ccdda9acb3b52a4e2fc4886
psterner <psterner@hotmail.com>
2023-10-19 16:47:20 +0200

    Add header en remove nav tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 export const Header = () => {
   return (
     <>
-      <img
-        className="play-icon"
-        src="./src/assets/icons/menu-icon.svg"
-        alt="Menu-Icon"
-        width="50"
-        height="50"
-      />
-      <h1>Music Releases from </h1>
-      <img
-        className="spotify-icon"
-        src="./src/assets/icons/spotify-icon.svg"
-        alt="Spotify-Icon"
-        width="40"
-        height="40"
-      />
+      <header>
+        <img
+          id="menu-icon"
+          className="menu-icon"
+          src="./src/assets/icons/menu-icon.svg"
+          alt="Menu-Icon"
+          width="40"
+          height="40"
+        />
+        <img
+          id="close-icon"
+          className="close-icon"
+          src="./src/assets/icons/close-icon.svg"
+          alt="Close-Icon"
+          width="40"
+          height="40"
+        />
+        <h1>Music Releases from </h1>
+        <img
+          className="spotify-icon"
+          src="./src/assets/icons/spotify-icon.svg"
+          alt="Spotify-Icon"
+          width="40"
+          height="40"
+        />
+      </header>
     </>
   );
 };
