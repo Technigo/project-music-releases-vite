81377f03a6aa7df043ed31eb923d3010d5760599
Emelie Segell <segell.emelie@gmail.com>
2023-10-20 21:21:41 +0200

    create album name and artist name components

---
+++ src/components/AlbumName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AlbumName = () => {
+  return <div>AlbumName</div>;
+};
+
+export default AlbumName;
---
+++ src/components/ArtistName.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtistName = () => {
+  return <div>ArtistName</div>;
+};
+
+export default ArtistName;
