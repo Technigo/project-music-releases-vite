6e47f0ce1cefa748180bae12471eccdb86487110
psterner <psterner@hotmail.com>
2023-10-20 14:54:37 +0200

    Delete unused file

--- src/components/Controller/PlayControll.jsx
+++ src/components/Controller/PlayControll.jsx
@@ -1,14 +1,13 @@
 import HeartIcon from "../../assets/icons/heart.svg";
 import PlayIcon from "../../assets/icons/play.svg";
 import DotsIcon from "../../assets/icons/dots.svg";
-import "./PlayControll.css";
 
 export const PlayControll = () => {
   return (
     <div className="playcontroll__flex_container">
       <div className="playcontroll__image_div playcontroll__image_div_heart">
         <img src={HeartIcon} alt="heart icon" width="100%" />
-      </div> 
+      </div>
       <div className="playcontroll__image_div playcontroll__image_div_play">
         <img src={PlayIcon} alt="play icon" width="100%" />
       </div>
