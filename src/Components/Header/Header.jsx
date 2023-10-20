d64e383cfcd310f48d49a080738e328312150719
Linnea Johansson <Linneajoh@gmail.com>
2023-10-20 10:45:19 +0200

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@
 export const Header = () => {
   return (
     <div className="header">
-        <h1 className="header-text">New Music Releases</h1>
+        <h1 className="header-text">Music Releases</h1>
     </div>
   )
 }
