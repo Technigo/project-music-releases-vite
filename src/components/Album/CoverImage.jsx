dca3a981aa355860ad355539836805449fc6ac40
Usha Lingamallu <lingamalluusha@gmail.com>
2023-10-21 16:55:01 +0200

    iconfix

--- src/components/Album/CoverImage.jsx
+++ src/components/Album/CoverImage.jsx
@@ -4,10 +4,10 @@ const CoverImage = ({ albumName, url }) => {
       <img className="coverImage" src={url} alt={albumName} />
       <span className="iconWrapper">
         <button type="button" className="heartIcon">
-          <img src="../../src/assets/icons/heart.svg" />
+          <img src="../src/assets/icons/heart.svg" />
         </button>
         <button type="button" className="playIcon">
-          <img src="../../src/assets/icons/play.svg" />
+          <img src="../src/assets/icons/play.svg" />
         </button>
         <button type="button" className="dotsIcon">
           <img src="../../src/assets/icons/dots.svg" />
