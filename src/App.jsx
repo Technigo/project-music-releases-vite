cc079d6a608f92ca3479f651c2927a2ae5e5d1e5
Kris Albert Lee <krisalbert@gmail.com>
2023-10-21 22:57:55 +0200

    add SidebarLinks component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import { useState } from 'react';
 import { Album } from './components/Album';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
-import data from './data.json';
+import { headerSinglesAlbums } from './constants';
+import { albums } from './data.json';
 import { playlists } from './stretched-goal.json';
 
 export const App = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [albumData, setAlbumData] = useState(albums.items);
+  const [headerName, setHeaderName] = useState(headerSinglesAlbums);
   return (
     <div>
       <Sidebar
         isNavOpen={isNavOpen}
         setIsNavOpen={setIsNavOpen}
         playlists={playlists.items}
+        setAlbumData={setAlbumData}
+        albumData={albums.items}
+        setHeaderName={setHeaderName}
+        headerName={headerName}
       />
       <div className="main-body">
         <div
@@ -21,7 +28,7 @@ export const App = () => {
         >
           <Header text="New Albums & Singles" />
           <div className="album-collection">
-            {data.albums.items.map(album => {
+            {albumData.map(album => {
               return (
                 <div key={album.id} className="album">
                   <Album album={album} />
