a6c3c01ab20e86fe17d55d8d496d930458d6b7f2
Emelie Segell <segell.emelie@gmail.com>
2023-10-21 17:13:00 +0200

    artist name object

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,41 +1,41 @@
 import { PropTypes } from "prop-types";
 export const ArtistName = ({ artists }) => {
-  //   let artist = { name: [], uri: [] };
-  let name = [];
-  let uri = [];
+  let artist = {};
+  //   let name = [];
+  //   let uri = [];
 
-  //   artists.map((val) => {
-  //     name.push(val.name), uri.push(val.uri);
-  //   });
+  artists.map((val) => {
+    // name.push(val.name), uri.push(val.uri);
+  });
 
-  //   return (
-  //     <div>
-  //       <span key={key}>
-  //         <span artist={artist}>
-  //           <a href={artist.uri}>{artist.name}, </a>
-  //         </span>
-  //       </span>
-  //     </div>
-  //   );
-
-  artists.map((val) => (name.push(val.name), uri.push(val.uri)));
   return (
     <div>
-      {name.map((name, key) => (
-        <span key={key}>
-          <span name={name}>{name}, </span>
+      <span key={key}>
+        <span artist={artist}>
+          <a href={artist.uri}>{artist.name}, </a>
         </span>
-      ))}
-      {uri.map((uri, key) => (
-        <span key={key}>
-          <span uri={uri}>
-            <a href={uri}> ARTIST LINK</a>
-          </span>
-        </span>
-      ))}
-      ;
+      </span>
     </div>
   );
+
+  //   artists.map((val) => (name.push(val.name), uri.push(val.uri)));
+  //   return (
+  //     <div>
+  //       {name.map((name, key) => (
+  //         <span key={key}>
+  //           <span name={name}>{name}, </span>
+  //         </span>
+  //       ))}
+  //       {uri.map((uri, key) => (
+  //         <span key={key}>
+  //           <span uri={uri}>
+  //             <a href={uri}> ARTIST LINK</a>
+  //           </span>
+  //         </span>
+  //       ))}
+  //       ;
+  //     </div>
+  //   );
 };
 
 ArtistName.propTypes = {
