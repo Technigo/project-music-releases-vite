0aef160737941c03a7bb58d0b952afb8a308d8db
Kris Albert Lee <krisalbert@gmail.com>
2023-10-21 20:39:13 +0200

    fix props in cover-image

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,17 +1,14 @@
 import { PropTypes } from 'prop-types';
+
 import { AlbumName } from './AlbumName';
 import { CoverImage } from './CoverImage';
 
 export const Album = ({ album }) => {
-  // TODO: remove this later on
-  // console.log(album);
-
   const image = album.images.find(image => image.height === 640);
-
   return (
     <div className="album-container">
-      <CoverImage img={image.url} alt={album.name} />
-      {<AlbumName albumName={album.name} uri={album.uri} />}
+      <CoverImage withHover img={image.url} name={album.name} />
+      <AlbumName albumName={album.name} uri={album.uri} />
     </div>
   );
 };
