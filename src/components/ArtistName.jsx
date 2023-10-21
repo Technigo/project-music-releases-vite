1a4ea011511e5fb3eaa61d0f720d18f065c5c7fe
Emelie Segell <segell.emelie@gmail.com>
2023-10-21 19:46:46 +0200

    add css for artist name

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,10 +1,12 @@
 import { PropTypes } from "prop-types";
+import "./ArtistName.css";
+
 export const ArtistName = ({ artists }) => {
   return (
     <div>
       {artists.map((artist) => (
-        <span key={artist.id}>
-          <a href={artist.external_urls.spotify}>{artist.name}, </a>
+        <span key={artist.id} className="artist-name-link">
+          <a href={artist.external_urls.spotify}>{artist.name} </a>
         </span>
       ))}
     </div>
