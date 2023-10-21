e330ad8576dc07f272aeedc8868c5064df396213
Kris Albert Lee <krisalbert@gmail.com>
2023-10-21 22:49:18 +0200

    add constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const headerSingles = 'New Singles';
+export const headerAlbums = 'New Albums';
+export const headerSinglesAlbums = 'New Singles and Albums';
+
+export const sidebarLinks = [
+  {
+    description: headerSingles,
+    type: 'single',
+  },
+  {
+    description: headerAlbums,
+    type: 'album',
+  },
+  {
+    description: headerSinglesAlbums,
+    type: '',
+  },
+];
