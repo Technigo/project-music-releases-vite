c81acb2fb98bbb16116ec56c98733c86f1c011a3
intehon <intehon@gmail.com>
2023-10-21 12:07:56 +0200

    Remove icons overlay

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,15 +1,10 @@
 import { PropTypes } from 'prop-types';
+//import { HoverImage } from './HoverImage';
 
 export const CoverImage = (album) => {
     return (
-      <div className="overlay">
-        <div className="icon-container">
-          <img src="./src/assets/icons/heart.svg" className="heart icon" alt="heart"></img>
-          <img src="./src/assets/icons/play.svg" className="play icon" alt="play"></img>
-          <img src="./src/assets/icons/dots.svg" className="dots icon" alt="dots"></img>
-        </div>
-      <img src={album.img} className="album-image" alt={album.name} />
-   </div>
+        //HoverImage goes here
+            <img src={album.img} className="album-image" alt={album.name} />
   );
 };
 
