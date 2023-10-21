6b9558efc2d8e068e3ce385487356b2a642db151
Kris Albert Lee <krisalbert@gmail.com>
2023-10-21 16:51:32 +0200

    fix css for cover image, fix styling on files

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,11 +1,11 @@
-import { PropTypes } from "prop-types";
+import { PropTypes } from 'prop-types';
 import { CoverImage } from './CoverImage';
 
 export const Album = ({ album }) => {
   // TODO: remove this later on
   console.log(album);
 
-  const image = album.images.find(image => image.height === 640)
+  const image = album.images.find(image => image.height === 640);
 
   return (
     <div className="album-container">
