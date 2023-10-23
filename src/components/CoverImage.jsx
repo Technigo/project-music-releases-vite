7e1ec2bfa03c00731ef8778903eb04b861462148
Emma Corazza <143932219+EmmaCorazza@users.noreply.github.com>
2023-10-23 17:40:48 +0200

    Add files via upload

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -6,13 +6,9 @@ export const CoverImage = ({ image }) => {
       <div className="CoverImage">
         <img src={image} alt="Album Cover"></img>
         <div className="icons">
-          <Icon
-            src="/src/assets/icons/heart.svg"
-            alt="Heart"
-            className="Heart"
-          />
-          <Icon src="./src/assets/icons/play.svg" alt="Play" className="Play" />
-          <Icon src="./src/assets/icons/dots.svg" alt="Dots" className="Dots" />
+          <Icon src="./assets/icons/heart.svg" alt="Heart" className="Heart" />
+          <Icon src="./assets/icons/play.svg" alt="Play" className="Play" />
+          <Icon src="./assets/icons/dots.svg" alt="Dots" className="Dots" />
         </div>
       </div>
     </>
