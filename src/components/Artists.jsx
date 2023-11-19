f3ee73301d9c2e8d485434bccb66990011d5381e
BeckieMorton <beckam11@gmail.com>
2023-11-19 15:48:19 +0100

    removed styling for artists link

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -11,7 +11,7 @@ export const Artists = ({ artists }) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              {(index ? " , " : "") + art.name}
+              {(index ? ", " : "") + art.name}
             </a>
           </span>
         );
