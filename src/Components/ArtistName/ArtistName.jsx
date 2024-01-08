878791eac4979e0fb2c610a5377f6473f4b70705
Linnea Johansson <Linneajoh@gmail.com>
2024-01-08 17:52:59 +0100

    fixed a link to artists

--- src/Components/ArtistName/ArtistName.jsx
+++ src/Components/ArtistName/ArtistName.jsx
@@ -1,39 +1,27 @@
 export const ArtistName = ({ singleArtistItem }) => {
   const { artists } = singleArtistItem;
-  let artistNames = "";
 
-  if (artists.length === 1) {
-    artistNames = artists[0].name;
-  } else if (artists.length === 2) {
-    artistNames = `${artists[0].name} & ${artists[1].name}`;
-  } else if (artists.length > 2) {
-    artists.map((artist, index) => { 
-      if (index < artists.length - 2) {
-        return (artistNames += `${artist.name}, `); 
-      } else if (index === artists.length - 2) {
-        return (artistNames += `${artist.name} & `);
-      } else {
-        return (artistNames += artist.name);
-      }
-    });
-  }
-  let artistObjects = {
-    artistName: artistNames,
-    aHref: singleArtistItem.artists[0].external_urls.spotify, 
-    target: "_blank", 
-    rel: "noreferrer noopener", 
-  }
-  
-    return (
-      <div className="artist-name">
-        <a 
-        href={artistObjects.aHref}
-        target={artistObjects.target}
-        rel={artistObjects.rel} 
-        >
-          <h3>{artistObjects.artistName}</h3>
+  const renderArtists = () => {
+    return artists.map((artist, index) => {
+      const separator = index < artists.length - 1 ? ' & ' : '';
+      return (
+        <span key={artist.id}>
+          <a 
+            href={artist.external_urls.spotify}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {artist.name}
           </a>
-  
-      </div>
-    )
-  }
+          {separator}
+        </span>
+      );
+    });
+  };
+
+  return (
+    <div className="artist-name">
+      <h3>{renderArtists()}</h3>
+    </div>
+  );
+};
