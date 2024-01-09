b125a3a32b3b53fb709bf82303af6be93966ad80
isaackamran <isaackamran@hotmail.com>
2024-01-09 23:51:20 +0100

    finished2

--- src/Components/icons.jsx
+++ src/Components/icons.jsx
@@ -4,9 +4,9 @@
      return (
        <div className="image-overlay">
          <div className="icons-container">
-           <img className="heart-icon" src="../public/icons/heart.svg" alt="Heart Icon" />
-           <img className="play-icon" src="../public/icons/play.svg" alt="Play Icon" />
-           <img className="dots-icon" src="../public/icons/dots.svg" alt="Dots Icon" />
+           <img className="heart-icon" src="../public/heart.svg" alt="Heart Icon" />
+           <img className="play-icon" src="../public/play.svg" alt="Play Icon" />
+           <img className="dots-icon" src="../public/dots.svg" alt="Dots Icon" />
          </div>
        </div>
      )
