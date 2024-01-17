4fcb44f14acc0731ca22e71359ae89fd683729f4
Olga Lepistö <olga.lepisto@gmail.com>
2024-01-17 13:18:11 +0200

    fixed the comma between artists issue

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -2,15 +2,17 @@ import "./Album.css";
 
 export const ArtistName = ({ artistName, artistLink }) => {
     if (artistName.length === 1) {
-    return (
-        <div className="artist-name">
-            <a href={artistLink} target="_blank"><p>{artistName}</p></a>
-        </div>
-    )
-} else if (artistName.length > 1) {
-    return (
-        <div className="artist-name">
-            <a href={artistLink} target="_blank"><p>{artistName}, {artistName}</p></a>
-        </div>
-    )}
-};
+        return (
+            <div className="artist-name">
+                <a href={artistLink} target="_blank"><p>{artistName}</p></a>
+            </div>
+        )
+
+    } else if (artistName.length > 1) {
+        return (
+            <div className="artist-name">
+                <a href={artistLink} target="_blank"><p>{artistName.join(', ')}</p></a>
+            </div>
+        );
+    }
+}
