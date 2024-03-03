5d965b9d77bfc7e6e2c71a3d77c954f55843b37c
Pernilla Sterner <psterner@hotmail.com>
2024-03-03 21:11:46 +0100

    Add sperators when more then two artists

--- src/components/Album/ArtistName.jsx
+++ src/components/Album/ArtistName.jsx
@@ -1,18 +1,31 @@
-export const ArtistName = (artists) => {
+import React from "react";
+
+export const ArtistName = (artists, index) => {
+  const allArtists = artists.artists.length;
+
   return (
     <>
-      {artists && (
+      {artists.artists && (
         <>
           {artists.artists.map((artist, index) => (
-            <a
-              key={index}
-              className="artist-name"
-              target="_blank"
-              href={artist.external_urls.spotify}
-            >
-              {index > 0 && ", "}
-              {artist.name}
-            </a>
+            // Using React.Fragment so I don´t need to add additional DOM element
+            <React.Fragment key={index}>
+              {allArtists > 1 && allArtists < 3 && index !== 0 && (
+                <span className="separator"> & </span>
+              )}
+              {allArtists > 2 && index !== 0 && (
+                <span className="separator">,</span>
+              )}
+
+              <a
+                className="artist-name"
+                target="_blank"
+                rel="noopener noreferrer" // Important for security when using target="_blank"
+                href={artist.external_urls.spotify}
+              >
+                {artist.name}
+              </a>
+            </React.Fragment>
           ))}
         </>
       )}
