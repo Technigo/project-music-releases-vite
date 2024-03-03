8e4ac9b6031979d44a2f66d86448b357609bc7b3
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-03-03 11:47:24 +0100

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import data from "./data.json";
-import {Album} from "./Album.jsx"
+import { Album } from "./Album.jsx";
+import { Header } from "./Header.jsx";
 
 console.log(data);
 
 export const App = () => {
   return (
-  <div className="wrapper">
-    {data.albums.items.map((album, i) => {
-      return(
-      <Album key={i} 
-      src={album.images[1].url}
-      albumTitle={album.name}
-      albumUrl={album.external_urls.spotify}
-      artists={album.artists}/>
-      )
-    })}
-  </div>
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main className="wrapper">
+        {data.albums.items.map((album, i) => {
+          return (
+            <Album
+              key={i}
+              src={album.images[1].url}
+              albumTitle={album.name}
+              albumUrl={album.external_urls.spotify}
+              artists={album.artists}
+            />
+          );
+        })}
+      </main>
+    </div>
   );
 };
---
+++ src/Header.jsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <h1>50 latest music releases</h1>;
+};
