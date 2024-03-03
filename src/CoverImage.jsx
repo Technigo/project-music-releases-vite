a5cdca48515c09fef1e9327cb41378998454472a
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-03-03 16:32:41 +0100

    Bug fix - imported icons

--- src/CoverImage.jsx
+++ src/CoverImage.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
+import heart from "./assets/icons/heart.svg";
+import dots from "./assets/icons/dots.svg";
+import play from "./assets/icons/play.svg";
 
 export const CoverImage = ({src}) => {
   return (
     <div className="cover-img">
       <img src={src} alt=""  />
       <div className="hover">
-        <img className="hover-heart" src="./src/assets/icons/heart.svg"/>
-        <img className="hover-play" src="./src/assets/icons/play.svg"/>
-        <img className="hover-dots" src="./src/assets/icons/dots.svg"/>
+        <img className="hover-heart" src={heart}/>
+        <img className="hover-play" src={play}/>
+        <img className="hover-dots" src={dots}/>
       </div>
     </div>
   )
