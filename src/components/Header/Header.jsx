17771b95df0df25a63232210487bcba807816133
Pernilla Sterner <psterner@hotmail.com>
2024-03-04 11:00:57 +0100

    Add heading and play icon for dropdown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
-import "./Header";
+import "./Header.css";
+import play from "../../assets/icons/play.svg";
 
 export const Header = (data) => {
   console.log(data);
-  return <header>Header</header>;
+  return (
+    <header>
+      <h1>Music Releases</h1>
+      <img src={play} alt="Play Icon" className="logo" />
+    </header>
+  );
 };
