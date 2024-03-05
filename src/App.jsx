491cd8f3cb5903871f9167203160a4d2c003c858
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-05 20:43:58 +0100

    Created folder structure and files. Added names to albums.
    Co-authored-by: Wen Z <wwenzz@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import data from "./data.json";
+import { Album } from "./components/Album";
+
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const { albums } = data;
+  const renderAlbum = albums.items.map(({ name }) => <Album name={name} />)
+  console.log(albums)
+  return <div>Find me in src/app.jsx!
+    {renderAlbum}
+
+  </div>;
 };
---
+++ src/components/Album.jsx
@@ -0,0 +1,7 @@
+export const Album = ({ name }) => {
+    return (
+        <div className="album">
+            <p>Name: {name}</p>
+        </div>
+    )
+}
