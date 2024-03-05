4a9d032049691091207872ec00dc438bea6a8857
Your El1an3 <eliane_w@yahoo.com>
2024-03-05 15:38:20 +0100

    changed dark overlay, brought icons to the foreground

--- src/assets/Components/CoverImage/CoverImage.jsx
+++ src/assets/Components/CoverImage/CoverImage.jsx
@@ -1,18 +1,19 @@
-import './CoverImage.css'
+import "./CoverImage.css";
 
-import heartIcon from './icons/heart.svg'
-import playIcon from './icons/play.svg'
-import dotsIcon from './icons/dots.svg'
+import heartIcon from "./icons/heart.svg";
+import playIcon from "./icons/play.svg";
+import dotsIcon from "./icons/dots.svg";
 export const CoverImage = ({ image, alt }) => {
   return (
     // added classname
     <div className="container">
       <img className="cover-image" src={image} alt={alt} />
+      <div className="overlay"></div>
       <div className="icons">
         <img id="icon-small" src={heartIcon} alt="heart-icon" />
         <img id="icon-big" src={playIcon} alt="play-icon" />
         <img id="icon-small" src={dotsIcon} alt="dots-icon" />
       </div>
     </div>
-  )
-}
+  );
+};
