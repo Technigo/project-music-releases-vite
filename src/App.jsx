5150e8fdbb165fa7e386cee5d83c6e5f4f6fbc95
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-05 23:09:07 +0100

    Make the grid of album colletion, flexbox of each album element and style the border and the background.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import data from "./data.json";
 import { Album } from "./components/Album";
 
-
 console.log(data);
 
 export const App = () => {
   const { albums } = data;
-  const renderAlbum = albums.items.map(({ name, artists, id, images }) => <Album name={name} key={id} artists={artists} images={images} />)
-  console.log(albums)
-  return <div>Find me in src/app.jsx!
-    {renderAlbum}
-
-  </div>;
+  const renderAlbum = albums.items.map(({ name, artists, id, images }) => (
+    <Album name={name} key={id} artists={artists} images={images} />
+  ));
+  console.log(albums);
+  return (
+    <div className="album-collection">
+      {/* Find me in src/app.jsx! */}
+      {renderAlbum}
+    </div>
+  );
 };
