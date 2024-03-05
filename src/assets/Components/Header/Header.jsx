602c3e58be2633434dfdf9b69fefbfe45db1e167
Your El1an3 <eliane_w@yahoo.com>
2024-03-05 16:48:30 +0100

    small changes

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -2,8 +2,8 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-    <div>
-      <h1 className="header">New Releases</h1>
+    <div className="header">
+      <h1>New Releases</h1>
     </div>
   );
 };
