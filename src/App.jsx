c1ae00de0a86b1896c5e55dbf2c54b90e47101b4
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-05 19:01:38 +0100

    add Album structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import data from "./data.json";
+import { Fragment } from "react";
+import { Header } from "./Header";
+import { Album } from "./Album/Album"
 
-console.log(data);
-
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+export const App = ()=>{
+  return (
+    <Fragment>
+      <Header />
+      <Album />
+    </Fragment>
+  )
+}
