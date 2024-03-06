8fd521bd3e9f58f1a6859f0dad5351f44078f7ac
fridaforser <fridaforser@gmail.com>
2024-03-06 19:10:24 +0100

    Added a div and imported css stylesheet

--- src/assets/Components/Album/Album.jsx
+++ src/assets/Components/Album/Album.jsx
@@ -1,13 +1,13 @@
 import { AlbumName } from "./AlbumName/AlbumName.jsx";
 import { ArtistName } from "./ArtistName/ArtistName.jsx";
 import { CoverImage } from "./CoverImage/CoverImage.jsx";
-
+import "./Album.css";
 
 export const Album = ({albums}) => {
   return(
     <section className="album-section">
       {albums.map((album) => (
-        <div className="album" key={album.id}>
+        <div className="album-cards" key={album.id}>
           <CoverImage coverImage={album.images?.[0]?.url}/>
           <div className="album-info">
             <AlbumName 
