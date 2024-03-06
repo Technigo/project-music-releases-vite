ba7168cb7c00c46bf2904a934df7d0317751e9a6
fridaforser <fridaforser@gmail.com>
2024-03-06 12:15:00 +0100

    Imported Header and Album files, and added a container div with header and album in the app function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import data from "./data.json";
-
-console.log(data);
+import { Header } from "./Header.jsx";
+import { Album } from "./assets/Components/Album/Album.jsx";
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return ( 
+    <div className="container">
+    <Header />
+    <Album albums={data.albums.items}/>
+  </div>
+  )
 };
