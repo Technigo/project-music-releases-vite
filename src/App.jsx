d71de5357d6024e19f33d3e44e58333b208ee23e
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-06 20:42:45 +0100

    Add comments on code and change name.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import data from "./data.json";
 
 console.log(data);
 
+// This is a React component, which is a function that returns JSX. 
 export const App = () => {
+
   return (
     <div>
-      
+      //Map is the same thing as for-loop, that you use to render through the array of data. 
       {data.albums.items.map((album) => (
-        <div className="card">
+        <div className="album">
           <p>{album.name}</p>
          
         </div>
