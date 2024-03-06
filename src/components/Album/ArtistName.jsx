7af0c6d3f3a7826d6aab52ed88b0d4517ab59d2f
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-03-06 16:41:02 +0100

    Chaged a typo in ArtistName.

--- src/components/Album/ArtistName.jsx
+++ src/components/Album/ArtistName.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export const ArtistName = ({ artists }) => {
   const artistNames = artists.map((artist) => artist.name).join(", ");
 
-  return <div className="artist-names">{artistNames}</div>;
+  return <div className="artistNames">{artistNames}</div>;
 };
 
 ArtistName.propTypes = {
