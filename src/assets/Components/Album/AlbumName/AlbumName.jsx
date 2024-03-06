68ce3da963c77943d8f5dcb929221496febf2747
fridaforser <fridaforser@gmail.com>
2024-03-06 12:09:10 +0100

    Created an AlbumName folder with jsx and css files and created an AlbumName function

---
+++ src/assets/Components/Album/AlbumName/AlbumName.jsx
@@ -0,0 +1,11 @@
+export const AlbumName = ({ albumName, albumURL }) => {
+  return(
+    <div className="album-name">
+      <a href={albumURL}
+      target="_blank"
+      className="album-link">
+          <h2>{albumName}</h2>
+      </a>
+    </div>
+  )
+}
