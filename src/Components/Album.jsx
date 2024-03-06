ff632791ba8bf75945300aed6882e88c84860968
SofieFerrari <sofiestrahl@gmail.com>
2024-03-06 22:46:07 +0100

    added styling to album-wrapper

--- src/Components/Album.jsx
+++ src/Components/Album.jsx
@@ -1,24 +1,29 @@
-import PropTypes from "prop-types"
-import { AlbumName } from "./AlbumName";
-import { ArtistName } from "./ArtistName";
-import { CoverImage } from "./CoverImage";
+import PropTypes from 'prop-types'
+import { AlbumName } from './AlbumName'
+import { ArtistName } from './ArtistName'
+import { CoverImage } from './CoverImage'
+import './Album.css'
 
-
-export const Album = ({albumName, coverImg, artists}) => {
-    return (
-        <div className="album-card">
-            <div className="album-container">
-                <CoverImage coverImg={coverImg}/>
-                <AlbumName albumName={albumName} />
-                <ArtistName artists={artists}/>
-            </div>
-        </div>
-    )
+export const Album = ({ albumName, coverImg, artists }) => {
+	return (
+		<section className='album-wrapper'>
+			<div className="album-card">
+				<div className="album-container">
+					{/* Accessing album cover */}
+					<CoverImage coverImg={coverImg} />
+					{/* Accessing album name */}
+					<AlbumName albumName={albumName} />
+					{/* Accessing artist name */}
+					<ArtistName artists={artists} />
+				</div>
+			</div>
+		</section>
+	)
 }
 
 //Define proptypes
 Album.propTypes = {
-    albumName: PropTypes.string.isRequired,
-    coverImg: PropTypes.string.isRequired,
-    artists: PropTypes.arrayOf(PropTypes.string).isRequired
-};
+	albumName: PropTypes.string.isRequired,
+	coverImg: PropTypes.string.isRequired,
+	artists: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
