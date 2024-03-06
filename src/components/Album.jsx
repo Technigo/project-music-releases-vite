56f813e18d3bed7d372e5b7aa134e7c9ba63f8e6
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-03-06 14:34:28 +0100

    Edited artistDatas.map

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,22 +1,26 @@
-//import
-// import { ArtistName } from "./ArtistName";
-// import { CoverImage } from "./CoverImage";
-
-import { AlbumName } from "./AlbumName"
+import { AlbumName } from "./AlbumName";
 import { ArtistName } from "./ArtistName";
-import { CoverImage } from "./CoverImage"
+import { CoverImage } from "./CoverImage";
 
-export const Album =({albumData}) =>{
-    const artistDatas = albumData.artists;
-    return (
-        <div>
-            <CoverImage coverImage={albumData.images[1].url} /> 
-            <AlbumName albumName={albumData.name} albumLink={albumData.external_urls.spotify} />
-            {artistDatas.map ((name,external_url)=>(
-                <ArtistName key = {artistDatas.id} artistName={name} artistLink={external_url} />
-            ))}
-        </div>
+export const Album = ({ albumData }) => {
+  const artistDatas = albumData.artists;
+  return (
+    <div>
+      <CoverImage coverImage={albumData.images[1].url} />
+      <AlbumName
+        albumName={albumData.name}
+        albumLink={albumData.external_urls.spotify}
+      />
+      {artistDatas.map((artist) => (
+        <ArtistName
+          key={artist.id}
+          name={artist.name}
+          external_url={artist.external_urls.spotify}
+        />
+      ))}
+    </div>
+  );
+};
 
-    )
-}
 //key = {albumData.images} for images?
+//it map() the items array, artist and images need to be map in Album component. key?
