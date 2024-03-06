7d6ece0c0e2933b53f1a290a31c934c8a28808ec
fridaforser <fridaforser@gmail.com>
2024-03-06 12:11:12 +0100

    Created a parent Album folder with jsx and css files and created an Album function that imports data from the child fragments and displays them in a section

---
+++ src/assets/Components/Album/Album.jsx
@@ -0,0 +1,24 @@
+import { AlbumName } from "./AlbumName/AlbumName.jsx";
+import { ArtistName } from "./ArtistName/ArtistName.jsx";
+import { CoverImage } from "./CoverImage/CoverImage.jsx";
+
+
+export const Album = ({albums}) => {
+  return(
+    <section className="album-section">
+      {albums.map((album) => (
+        <div className="album" key={album.id}>
+          <CoverImage coverImage={album.images?.[0]?.url}/>
+          <div className="album-info">
+            <AlbumName 
+              albumName={album.name} 
+              albumURL={album.external_urls.spotify}/>
+            <ArtistName 
+              artistName={album.artists.map((artist) => artist.name)}    
+              artistURL={album.artists.map((artist) => artist.external_urls.spotify)}/>  
+          </div>
+        </div>
+     ))}
+    </section>
+  )
+}
