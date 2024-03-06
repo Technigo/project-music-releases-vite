4dd187333ee7854c4f9cc4f8a601160212c4fc7f
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-06 21:31:13 +0100

    Add artists.

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,9 +1,15 @@
+import { ArtistName } from "./ArtistName";
 //Component (function in react). Working with bigger amounts of code, you need to work in diffrent files (components).
 export const Album = (props) => {
   return (
     <div>
       <p>{props.album.name}</p>
       <img src={props.album.images[0].url} alt={props.album.name} />
+      {/* Here we are rendering on album at a time, to get the array of all the artists. Its the same thing as book of books, that I did in the library project.  */}
+      {props.album.artists.map((artist) => (
+        // This is the component that gives back the information about the artists. 
+        <ArtistName artist={artist} />
+      ))}
     </div>
   );
 };
---
+++ src/ArtistName.jsx
@@ -0,0 +1,10 @@
+//Component (function in react). Working with bigger amounts of code, you need to work in diffrent files (components).
+export const ArtistName = (props) => {
+    return (
+      <div>
+        <p>{props.artist.name}</p>
+        
+      </div>
+    );
+  };
+  
