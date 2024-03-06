54a624dfe2ffd397665816eb571e2fcff576bc4d
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-06 21:37:39 +0100

    Completed header styling with toggle button to separate albums and singles.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 export const Header = () => {
-
-    return (
-        <div className="header">
-            <h1>Moon Music </h1>
-        </div>
-
-    )
-}
+  return (
+    <div className="header">
+      <h1>Moon Music </h1>
+      <label className="switch">
+        <input type="checkbox"></input>
+        <span className="slider round"></span>
+      </label>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import { Header } from "./components/Header.jsx"
 import "./index.css";
+import "./components/header.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
