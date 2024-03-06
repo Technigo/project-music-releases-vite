a55f4d52e336589d48aff9621469f8b0371565e8
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-06 18:06:03 +0100

    updated Album.jsx

--- src/album/Album.jsx
+++ src/album/Album.jsx
@@ -1,3 +1,15 @@
 import { AlbumName } from "./AlbumName";
 import { ArtistName } from "./ArtistName";
 import { CoverImage } from "./CoverImage";
+
+export const Album = ({albumName, imgSrc, artists}) => {
+    return (
+        <div className="album-card">
+            <div className="album-container">
+                <CoverImage imgSrc={imgSrc}/>
+                <AlbumName albumName={albumName} />
+                <ArtistName artists={artists}/>
+            </div>
+        </div>
+    )
+}
