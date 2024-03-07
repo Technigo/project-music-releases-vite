31f54247797992f369911fac09bcaeb4bc870e8a
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-07 12:17:25 +0100

    fixed img size and , between artist names

--- src/Components/ArtistName.jsx
+++ src/Components/ArtistName.jsx
@@ -2,11 +2,15 @@ import PropTypes from "prop-types"
 import './Album.css'
 
 export const ArtistName = ({artists}) => {
+    const artistNames = artists.map(artist => artist.name).join(", ");
+
     return  (
         <div className="artist-names">
-            {artists.map(({ id, name}) => {
-                return <h3 key={id}>{name}</h3>
-            })}
+            <h3>{artistNames}</h3>
+            {/* tog bort detta för att lägga dit join() för att separera artister med , - men vet inte om "id" behövs?
+             {artists.map(({ id, name}) => {
+                return <h3 key={id}>{artistNames}</h3>
+            })} */}
         </div>
 
     )
