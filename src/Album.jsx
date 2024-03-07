1e5efa903d6f92dae2b0695bfb3787237ab1518f
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-07 20:13:20 +0100

    Create links for the albums.

--- src/Album.jsx
+++ src/Album.jsx
@@ -3,11 +3,16 @@ import { ArtistName } from "./ArtistName";
 export const Album = (props) => {
   return (
     <div>
-      <p>{props.album.name}</p>
       <img src={props.album.images[0].url} alt={props.album.name} />
+      {/* The a element: the props is the album, we are getting the album out of the object, to get to urls. */}
+      <a href={props.album.external_urls.spotify}>
+        {/* And this line is to make the link as the text of the album. */}
+        {props.album.name}
+      </a>
+
       {/* Here we are rendering on album at a time, to get the array of all the artists. Its the same thing as book of books, that I did in the library project.  */}
       {props.album.artists.map((artist) => (
-        // This is the component that gives back the information about the artists. 
+        // This is the component that gives back the information about the artists.
         <ArtistName artist={artist} />
       ))}
     </div>
