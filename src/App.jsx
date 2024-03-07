cbea7b5f2f45655164b4c9c1f90f85120e825a4e
schouenkes <m.schouenke@gmail.com>
2024-03-07 14:42:46 +0100

    added external urls to artist name

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ import { Album } from "./Components/Album.jsx";
 
 export const App = () => {
   const albums = data.albums.items;
-  const renderAlbums = albums.map(({ artists, images, name }, index, external_urls) => (
-    <Album key={index} coverImages={images} albumName={name} artists={artists} href={external_urls} />
+  const renderAlbums = albums.map(({ artists, images, name, external_urls, href }, index) => (
+    <Album
+      key={index}
+      coverImages={images}
+      albumName={name}
+      artists={artists}
+      href={external_urls.spotify}
+      artistshref={artists.external_urls}
+    />
   ));
   return (
     <div>
--- src/Components/ArtistName.jsx
+++ src/Components/ArtistName.jsx
@@ -1,4 +1,10 @@
 export const ArtistName = (props) => {
-  const renderArtists = props.artists.map((artist) => artist.name).join(", ");
-  return <p className="artist-name">{renderArtists}</p>;
+  const renderArtists = props.artists.map((artist, index) => (
+    <a key={artist.id} href={artist.external_urls.spotify} className="artist-name">
+      {artist.name}
+      {index < props.artists.length - 1 ? ", " : ""}
+    </a>
+  ));
+
+  return <>{renderArtists}</>;
 };
