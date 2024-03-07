9eab4464395fedbfe10f27b9cf088cc50a774186
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-07 19:29:06 +0100

    added hovering styling and icons

--- src/Components/CoverImage.jsx
+++ src/Components/CoverImage.jsx
@@ -4,26 +4,32 @@ import Heart from "../assets/icons/heart.svg"
 import Play from "../assets/icons/play.svg"
 import Dots from "../assets/icons/dots.svg"
 
-
 import "./Album.css"
 
-export const CoverImage = ({ coverImg }) => {
+export const CoverImage = ({ coverImg, albumUrl }) => {
   return (
     <div className="cover-image-container">
       <div className="cover-image">
         <img src={coverImg} alt="album cover" />
       </div>
-     <div className="icon-container">
-      <Icon icon={Heart}/>
-      <Icon icon={Play}/>
-      <Icon icon={Dots}/>
-     </div>
+      <div className="icon-container">
+        <Icon icon={Heart} />
+        <a
+          className="play-icon"
+          href={albumUrl}
+          target="_blank"
+          rel="noopener noreferrer">
+          <Icon icon={Play} />
+        </a>
+        <Icon icon={Dots} />
+      </div>
     </div>
   )
 }
 
 CoverImage.propTypes = {
   coverImg: PropTypes.string.isRequired,
+  albumUrl: PropTypes.string.isRequired,
 }
 
 export default CoverImage
