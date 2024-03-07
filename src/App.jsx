7e7c4ba4ebdbf03bef048451d35f535238ae3c94
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-07 16:59:59 +0100

    Started on sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import data from "./data.json";
 import { Album } from "./components/Album";
 import { Header } from "./components/Header.jsx";
+import { SortAlbums } from "./components/Sort.jsx";
+import { SortSingles } from "./components/Sort.jsx";
 import "./components/header.css";
 
 export const App = () => {
   const { albums } = data;
-  const renderAlbum = albums.items.map(
-    ({ name, artists, id, images, external_urls }) => (
-      <Album
-        name={name}
-        key={id}
-        artists={artists}
-        images={images}
-        external_urls={external_urls}
-      />
-    )
-  );
+
+  const renderAlbum = (arr) => {
+    const renderedAlbum = arr.map(
+      ({ name, artists, id, images, external_urls }) => {
+        return (
+          <Album
+            name={name}
+            key={id}
+            artists={artists}
+            images={images}
+            external_urls={external_urls}
+          />
+        );
+      }
+    );
+    return renderedAlbum;
+  };
+
   return (
     <>
       <Header />
-      <div className="album-collection">{renderAlbum}</div>
+      <div className="all-items album-collection">
+        {renderAlbum(albums.items)}
+      </div>
+      <div className="only-albums album-collection">
+        <div className="album album-type">
+          <h2>Albums</h2>
+        </div>
+        {renderAlbum(SortAlbums())}
+      </div>
+      <div className="only-singles album-collection">
+        <div className="album album-type">
+          <h2>Singles</h2>
+        </div>
+        {renderAlbum(SortSingles())}
+      </div>
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import { Sidebar } from "./header-components/Sidebar";
 
 export const Header = () => {
+
+  let singles = document.querySelector(".only-singles")
+  console.log(singles)
+
   return (
     <div className="header">
       <Sidebar />
-      <h1>Moon Music </h1>
+      <h1>Moon Music</h1>
       <label className="switch">
         <input type="checkbox"></input>
         <span className="slider round"></span>
