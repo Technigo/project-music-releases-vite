d5be2b529e01238b96c662c26c52c2aefde90590
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-07 20:34:05 +0100

    Add key to loop (feedback from console)

--- src/Album.jsx
+++ src/Album.jsx
@@ -3,17 +3,19 @@ import { ArtistName } from "./ArtistName";
 export const Album = (props) => {
   return (
     <div>
-      <img src={props.album.images[0].url} alt={props.album.name} />
+      {/* [0] This means first image. alt text for screen readers is set to an empty string otherwise it will repeat "album, album " */}
+      <img src={props.album.images[0].url} alt="" />
       {/* The a element: the props is the album, we are getting the album out of the object, to get to urls. */}
       <a href={props.album.external_urls.spotify}>
         {/* And this line is to make the link as the text of the album. */}
         {props.album.name}
       </a>
 
-      {/* Here we are rendering on album at a time, to get the array of all the artists. Its the same thing as book of books, that I did in the library project.  */}
+      {/* Here we are rendering one album at a time, to get the array of all the artists. Its the same thing as book of books, that I did in the library project.  */}
       {props.album.artists.map((artist) => (
-        // This is the component that gives back the information about the artists.
-        <ArtistName artist={artist} />
+        // This is the component that gives back the information about the artists/prints the artist name. 
+        // Add another key, because console said to do so.
+        <ArtistName artist={artist} key={artist.id}/>
       ))}
     </div>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ console.log(data);
 export const App = () => {
   return (
     <div>
-      // Map is the same thing as for-loop, that you use to render through the array of data.
+     {/* Map is the same thing as for-loop, that you use to render through the array of data. */}
       {data.albums.items.map((album) => (
-        <Album album={album} />
+        // Console.log showed a warning about the loop missing a key. Imagine that files on your desktop didn’t have names. Instead, you’d refer to them by their order — the first file, the second file, and so on. You could get used to it, but once you delete a file, it would get confusing. The second file would become the first file, the third file would be the second file, and so on.
+
+// File names in a folder and JSX keys in an array serve a similar purpose. They let us uniquely identify an item between its siblings. A well-chosen key provides more information than the position within the array. Even if the position changes due to reordering, the key lets React identify the item throughout its lifetime.
+        <Album album={album} key={album.id} />
       ))}
     </div>
   );
