b08ee97bd3e0bfe9d8b5806c9dc91a204c5f559a
Trista Shan <trista8703@hotmail.com>
2024-03-08 13:41:34 +0200

    deleted one comment

--- src/components/AlbumName.jsx
+++ src/components/AlbumName.jsx
@@ -6,9 +6,7 @@ export const AlbumName = ({albumName,albumLink}) =>{
     }
     return (
        <div className = "album-name">
-          {/* <p ><a href= {albumLink} >{albumName}</a></p> */}
           <p onClick= {clickAlumName}>{albumName}</p>
-            
        </div>
     )
 }
