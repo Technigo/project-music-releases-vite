75748b1ac768e08f59dea43916c5e6ca2b240be7
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-08 20:50:06 +0100

    Extracted album name into its own component.

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,4 +1,5 @@
 import { ArtistName } from "./ArtistName";
+import { AlbumName } from "./AlbumName";
 //Component (function in react). Working with bigger amounts of code, you need to work in diffrent files (components).
 export const Album = (props) => {
   return (
@@ -6,16 +7,13 @@ export const Album = (props) => {
       {/* [0] This means first image. alt text for screen readers is set to an empty string otherwise it will repeat "album, album " */}
       <img src={props.album.images[0].url} alt="" />
       {/* The a element: the props is the album, we are getting the album out of the object, to get to urls. */}
-      <a href={props.album.external_urls.spotify}>
-        {/* And this line is to make the link as the text of the album. */}
-        {props.album.name}
-      </a>
+      <AlbumName album={props.album} />
 
       {/* Here we are rendering one album at a time, to get the array of all the artists. Its the same thing as book of books, that I did in the library project.  */}
       {props.album.artists.map((artist) => (
-        // This is the component that gives back the information about the artists/prints the artist name. 
+        // This is the component that gives back the information about the artists/prints the artist name.
         // Add another key, because console said to do so.
-        <ArtistName artist={artist} key={artist.id}/>
+        <ArtistName artist={artist} key={artist.id} />
       ))}
     </div>
   );
--- src/AlbumName.jsx
+++ src/AlbumName.jsx
@@ -1,7 +1,11 @@
 export const AlbumName = (props) => {
   return (
     <div>
-      <p>{props.album.name}</p>
+      {/* The a element: the props is the album, we are getting the album out of the object, to get to urls. */}
+      <a href={props.album.external_urls.spotify}>
+        {/* And this line is to make the link as the text of the album. */}
+        {props.album.name}
+      </a>
     </div>
   );
-}
+};
