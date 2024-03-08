4d17e2601cac07a9b9c5e070de0e20b0cd815f0c
Citronskal <yiaswift@gmail.com>
2024-03-08 23:24:22 +0100

    css v6

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export const ArtistName = ({ artists }) => {
   return (
-    <p>
+    <span>
       {artists.map((artist, index) => (
-        <div key={index}>
+        <span key={index}>
           <a key={artist.id} href={artist.external_urls.spotify}>
             <span className="artist-name">
               {artist.name} {index === artists.length - 2 && " & "}
             </span>
           </a>
-        </div>
+        </span>
       ))}
-    </p>
+    </span>
   );
 };
