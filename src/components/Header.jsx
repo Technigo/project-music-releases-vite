93549375d0d1cd75a1e848f2a8028b6c78c6a4f8
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-08 09:25:55 +0100

    Animations stop when button is clicked.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import { Sidebar } from "./header-components/Sidebar";
 import PropTypes from "prop-types";
 
 export const Header = ({ sortOnClick }) => {
+  const [isActive, setActive] = useState("false");
+  // Makes a one-time toggle. To turn of animations when the user has noticed button.
+  const ToggleClass = () => {
+    if (isActive === "false") {
+      setActive(!isActive);
+    }
+  };
+
   return (
     <div className="header">
       <Sidebar />
       <h1>Moon Music</h1>
       <label className="switch">
-        <input type="checkbox" onChange={() => sortOnClick()}></input>
-        <span className="slider round"></span>
+        <input
+          type="checkbox"
+          onChange={() => {
+            ToggleClass();
+            sortOnClick();
+          }}
+        ></input>
+        <span className={isActive ? "slider round animate" : "slider round"}></span>
       </label>
     </div>
   );
