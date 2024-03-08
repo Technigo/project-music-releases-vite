84652c0d502c611a450c27604267bf0567229d5a
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-08 10:08:18 +0100

    Fix comma between artists

--- src/Components/ArtistName.jsx
+++ src/Components/ArtistName.jsx
@@ -1,46 +1,38 @@
-import PropTypes from 'prop-types'
-import './Album.css'
+import PropTypes from "prop-types"
+import "./Album.css"
 
 export const ArtistName = ({ artists }) => {
-	console.log(artists)
+  console.log(artists)
 
-	//Variable for more than 1 artists
-	const multipleArtists = artists.length > 1
+  //Variable for more than 1 artists
+  const multipleArtists = artists.length > 1
 
-	//Map artist with the correct url
-	return (
-		<div className="artist-names">
-			{artists.map((artist, index) => (
-				<div className="separated-artists" key={artist.id}>
-					<a href={artist.external_urls.spotify} target="_blank">
-						<h3>{artist.name}</h3>
-					</a>
-					{index <= artist.length - 2 && (multipleArtists ? ', ' : '')}
-					{index <= artist.length - 2 && (multipleArtists ? ', ' : '')}
-				</div>
-			))}
-		</div>
-	)
-}
-
-/*return (
+  //Map artist with the correct url
+  return (
     <div className="artist-names">
-      {artists.map(({ id, name, external_urls }) => (
-        <a key={id} href={external_urls.spotify} className="artist-link">
-          <h3>{name}</h3>
-        </a>
+      {artists.map((artist, index) => (
+        <div className="artist-link" key={artist.id}>
+          <a
+            href={artist.external_urls.spotify}
+            target="_blank"
+            rel="noreferrer noopener">
+            <h3>{artist.name}</h3>
+          </a>
+          {index < artists.length - 2 && (multipleArtists ? ", " : "")}
+          {index === artists.length - 2 && (multipleArtists ? " & " : "")}
+        </div>
       ))}
     </div>
   )
-}*/
+}
 
 ArtistName.propTypes = {
-	artists: PropTypes.arrayOf(
-		PropTypes.shape({
-			id: PropTypes.number.isRequired,
-			name: PropTypes.string.isRequired,
-		})
-	).isRequired,
+  artists: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 }
 
 export default ArtistName
