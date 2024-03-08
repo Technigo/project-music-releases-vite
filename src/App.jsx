ebd5ca838be7f07039e9376f8ca7b2284f8f4eec
Your Name <you@example.com>
2024-03-08 21:09:03 +0100

    happy with this results, I will keep consol.loging

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import data from "./data.json";
+import { Album } from "./components/Album"
+import data from "./data.json"
 
-console.log(data);
-
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+console.log(data.albums.items.slice(0, 2))
+//console.log("Artist:", data.albums.items)
+export const App = () =>{
+return(
+  <div className="albums">
+    {data.albums.items.map(album => (
+      <Album
+      key={album.id}
+      artistName={album.artists[0].name}
+      coverImage={album.images}
+      albumName={album.name}
+    />
+  ))}
+  </div>
+)
+}
---
+++ src/components/Album.jsx
@@ -0,0 +1,7 @@
+export const Album = () => {
+  return (
+    <div>Album</div>
+  )
+}
+
+
