04e7c75d25d8159d07fc22ed5e93fa191910afcd
Your Name <you@example.com>
2024-03-08 22:20:17 +0100

    enougth for today, tomorrow more

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Album } from "./components/Album"
 import data from "./data.json"
 
-console.log(data.albums.items.slice(0, 2))
-//console.log("Artist:", data.albums.items)
+console.log(data.albums.items.slice(0, 2)) 
+
+//Console.log hero help here:
+console.log("Artist name: ", data.albums.items[0].artists[0].name)
+console.log("Cover image: ", data.albums.items[0].images[0].url)
+console.log("Album name: ", data.albums.items[0].name)
+
+
 export const App = () =>{
 return(
   <div className="albums">
