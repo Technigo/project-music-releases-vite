07781e0e2dad29e4b8c1f8f432f0fc5854d5661d
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-08 14:44:56 +0100

    changed casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import data from "./data.json";
-import { Header } from "./components/Header";
-import { Album } from "./components/Album";
-import { Footer } from "./components/Footer";
-import './App.css'
+import data from "./data.json"
+import { Header } from "./Components/Header"
+import { Album } from "./Components/Album"
+import { Footer } from "./Components/Footer"
+import "./App.css"
 
 //This is where we get the data from json
 export const App = () => {
-  const albums = data.albums.items;
+  const albums = data.albums.items
 
-//Map over albums and retrieve id, name (of album), image and artists
+  //Map over albums and retrieve id, name (of album), image and artists
   const renderAlbums = () =>
-      albums.map(({ id, name, images, artists, external_urls}) => (
-        <Album
+    albums.map(({ id, name, images, artists, external_urls }) => (
+      <Album
         key={id}
         albumName={name}
         albumUrl={external_urls.spotify}
         coverImg={images[0].url}
         artists={artists}
-        />
-      ));
+      />
+    ))
 
-    //Display header and music content on the page
-      const renderAlbumContent = renderAlbums()
-      return (
-        <div className="app">
-          <Header/>
-          <div className="main-section">{renderAlbumContent}</div>
-          <Footer/>
-        </div>
-      )
+  //Display header and music content on the page
+  const renderAlbumContent = renderAlbums()
+  return (
+    <div className="app">
+      <Header />
+      <div className="main-section">{renderAlbumContent}</div>
+      <Footer />
+    </div>
+  )
+}
 
-  }
-
-  export default App
+export default App
