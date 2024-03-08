c3b8fc939915c7d34c371d25d49541144ff85431
Pernilla Sterner <psterner@hotmail.com>
2024-03-08 21:21:38 +0100

    Feature lighthouse (#13)
    * Add explicit width depending on playlist or album size
    
    * Add width and heigth to social icons

--- src/components/AlbumContent/CoverImage.jsx
+++ src/components/AlbumContent/CoverImage.jsx
@@ -2,14 +2,23 @@ import PropTypes from "prop-types";
 import { PlayIcons } from "../AlbumContent/PlayIcons";
 
 export const CoverImage = ({ images, typeOfList }) => {
+  const imgWidth = typeOfList === "album" ? 300 : 90;
+
   const coverImage = {
     imgUrl: images,
     imgAlt: "Cover album image",
+    width: imgWidth,
+    heigth: "auto",
   };
 
   return (
     <div className="coverImage">
-      <img src={coverImage.imgUrl} alt="Cover album image" />
+      <img
+        src={coverImage.imgUrl}
+        alt="Cover album image"
+        width={coverImage.width}
+        height={coverImage.heigth}
+      />
       {typeOfList && typeOfList === "album" && <PlayIcons />}
     </div>
   );
--- src/components/AlbumContent/PlayIcons.jsx
+++ src/components/AlbumContent/PlayIcons.jsx
@@ -5,9 +5,27 @@ import dots from "../../assets/icons/dots.svg";
 export const PlayIcons = () => {
   return (
     <div className="play-icons-container">
-      <img className="play-icon heart" src={heart} alt="Heart Icon" />
-      <img className="play-icon play" src={play} alt="Play Icon" />
-      <img className="play-icon dots" src={dots} alt="Dots Icon" />
+      <img
+        className="play-icon heart"
+        src={heart}
+        alt="Heart Icon"
+        width={50}
+        height={50}
+      />
+      <img
+        className="play-icon play"
+        src={play}
+        alt="Play Icon"
+        width={50}
+        height={50}
+      />
+      <img
+        className="play-icon dots"
+        src={dots}
+        alt="Dots Icon"
+        width={50}
+        height={50}
+      />
     </div>
   );
 };
