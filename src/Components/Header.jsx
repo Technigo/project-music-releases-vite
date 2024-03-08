2857fb9c2a3fd7e25de4b3c188e4604f590150d3
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-03-08 15:02:08 +0100

    change casing for footer.css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,11 @@
 import "./Header.css"
 
 export const Header = () => {
-
-    return(
-        <header>
-            <h1 className="header">Music Release Project</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1 className="header">Music Release Project</h1>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
