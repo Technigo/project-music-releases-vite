b248177381ebe2d2d1d2102d3a3492d1107dad4c
idsab <idasabic@gmail.com>
2024-03-08 08:55:36 +0100

    fixed issue with cover img hover

--- src/components/children/AlbumCover.jsx
+++ src/components/children/AlbumCover.jsx
@@ -3,8 +3,8 @@ import { PlayIcons } from "./PlayIcons";
 export const AlbumCover = ({ images }) => {
   return (
     <div className="album-cover-container">
-      <PlayIcons />
       <img className="album-cover" src={images} alt="album-cover-image" />
+      <PlayIcons />
     </div>
   );
 };
