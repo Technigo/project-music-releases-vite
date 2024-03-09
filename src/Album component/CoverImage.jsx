e9334107e7769980a966edc50e08202a5c381dfd
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-03-09 09:53:28 +0100

    clean up

--- src/Album component/CoverImage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CoverImage = ({image}) => {
-    return <img src={image} alt="Album Cover"/>
-};
-
-export default CoverImage;
--- src/Album/Album.jsx
+++ src/Album/Album.jsx
@@ -4,19 +4,20 @@ import {ArtistName} from "./components/ArtistName"
 import {CoverImage} from "./components/CoverImage"
 
 export const Album = () =>{
-return (
-    <section className="album-section">  
-        {
-            data.albums.items.map((album)=>(
-                <div className="album-card" key={album.id}>
-                    <CoverImage album={album}/>
-                    <ul className="album-info">
-                        <li><AlbumName album={album} /></li>
-                        <li><ArtistName artists={album.artists}/></li>
-                    </ul>
-               </div>
-            ))
-        }
-   </section>
-)
+    
+    return (
+        <section className="album-section">  
+            {
+                data.albums.items.map((album)=>(
+                    <div className="album-card" key={album.id}>
+                        <CoverImage album={album}/>
+                        <ul className="album-info">
+                            <li><AlbumName album={album} /></li>
+                            <li><ArtistName artists={album.artists}/></li>
+                        </ul>
+                </div>
+                ))
+            }
+    </section>
+    )
 }
