5b9efd3346ce25dd8db4318b8b59c7464984f996
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-10 11:53:52 +0100

    updated README and removed buttons.

--- pull_request_template.md
+++
@@ -1,6 +0,0 @@
-## Netlify link
-[Our website for the project](https://kathinka-nathalie-music-release.netlify.app/)
-
-## Collaborators
-Add your collaborators here. Write their GitHub usernames in square brackets. If there's more than one, separate them with a comma, like this:
-[ohinathalie] [kathinka]
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,6 @@ export const Header = () => {
   return (
     <div className="header-wrapper">
       <h1 className="header">New Releases</h1>
-      <div className="nav-wrapper">
-        <button id="all" className="type-btn">All</button>
-        <button id="album" className="type-btn">Album</button>
-        <button id="single" className="type-btn">Single</button>
-      </div>
     </div>
   )
 }
