88d1a1b4d681e97b2a11e0b4d852344f117b5cc8
jinghuang <jinghuang0628@gmail.com>
2024-03-10 21:40:55 +0100

    SVG image for deploy

--- src/components/album/CoverImage.jsx
+++ src/components/album/CoverImage.jsx
@@ -1,5 +1,8 @@
 import './CoverImage.css'
 import PropTypes from 'prop-types'
+import dots from '/src/assets/icons/dots.svg'
+import heart from '/src/assets/icons/heart.svg'
+import play from '/src/assets/icons/play.svg'
 
 export const CoverImage=({images,name})=> {  
   const imgUrl = images[1].url  
@@ -10,9 +13,9 @@ export const CoverImage=({images,name})=> {
       <img src={imgUrl} alt={imgAlt} className='coverImg' />
       <div className='imgFunction'> 
         
-        <img src="src/assets/icons/heart.svg" className='heart'></img>
-        <img src="src/assets/icons/play.svg" className='play'></img>
-        <img src="src/assets/icons/dots.svg" className='dots'></img>
+        <img src={heart} className='heart'></img>
+        <img src={play} className='play'></img>
+        <img src={dots} className='dots'></img>
         
       </div>
     </div>
