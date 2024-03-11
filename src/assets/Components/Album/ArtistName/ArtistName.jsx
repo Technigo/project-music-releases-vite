d4bd15436a7bdcc3f66380580c65ea4dcdf8c9d1
fridaforser <fridaforser@gmail.com>
2024-03-11 12:23:27 +0100

    Formatting

--- src/assets/Components/Album/ArtistName/ArtistName.jsx
+++ src/assets/Components/Album/ArtistName/ArtistName.jsx
@@ -1,11 +1,9 @@
 export const ArtistName = ({ artistName, artistURL }) => {
-  return(
+  return (
     <div className="artist-name">
-      <a href={artistURL}
-      target="_blank"
-      className="artist-link">
-          <h3>{artistName}</h3>
+      <a href={artistURL} target="_blank" className="artist-link">
+        <h3>{artistName}</h3>
       </a>
     </div>
-  )
-}
+  );
+};
--- src/assets/Components/Album/CoverImage/CoverImage.jsx
+++ src/assets/Components/Album/CoverImage/CoverImage.jsx
@@ -3,20 +3,16 @@ import play from "./icons/playwhite.svg";
 import dots from "./icons/dotswhite.svg";
 
 export const CoverImage = ({ coverImage }) => {
-    return ( 
+  return (
     <div className="album-icons">
-    <div className="cover-icons">
-    <img className="cover-image" src={coverImage}/>
-    <div className="icons">
-            <img className="heart" src={heart} width="30px"/>
-            <img className="play" src={play} width="30px"/>
-            <img className="dots" src={dots} width="30px"/>
-        </div></div>
-    
-    
+      <div className="cover-icons">
+        <img className="cover-image" src={coverImage} />
+        <div className="icons">
+          <img className="heart" src={heart} width="30px" />
+          <img className="play" src={play} width="30px" />
+          <img className="dots" src={dots} width="30px" />
+        </div>
+      </div>
     </div>
-    )
-}
-    
-
-    
+  );
+};
