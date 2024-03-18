40fadaf5e5e7037a6fc79558e171bb89e4417f86
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-18 17:55:31 +0100

    formatting should be now on point. else blame prettier.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Header } from "./components/Header.jsx"
 import { Album } from "./components/Album.jsx"
-import data from "./data.json";
+import data from "./data.json"
 import "./components/styling/app.css"
 
 //pass json data to  *mArray* in Album
 export const App = () => {
-
-  return (
-    <div className="app-wrap">
-      <Header albumList={data} />
-      <Album musicArray={data} />
-
-    </div>
-  )
+	return (
+		<div className='app-wrap'>
+			<Header albumList={data} />
+			<Album musicArray={data} />
+		</div>
+	)
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import "./styling/header.css"
 
 export const Header = () => {
 	return (
-		<div className="header-wrapper">
-      <h1 className="header">New Releases</h1>
+		<div className='header-wrapper'>
+			<h1 className='header'>New Releases</h1>
 		</div>
 	)
 }
